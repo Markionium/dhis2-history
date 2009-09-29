a64ad1511972490c0a8ad5263e1c518f9556fe9b
Lars Helge Oeverland larshelge@gmail.com <Lars Helge Oeverland larshelge@gmail.com>
2009-09-29 19:21:39 +0200

    Unit tests now run.

--- dhis-2/dhis-services/dhis-service-patient/src/test/java/org/hisp/dhis/patient/PatientStoreTest.java
+++ dhis-2/dhis-services/dhis-service-patient/src/test/java/org/hisp/dhis/patient/PatientStoreTest.java
@@ -1,10 +1,44 @@
+/*
+ * Copyright (c) 2004-2009, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of the HISP project nor the names of its contributors may
+ *   be used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.hisp.dhis.patient;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNull;
 
 import org.hisp.dhis.DhisSpringTest;
 import org.junit.Test;
 
-import static junit.framework.Assert.*;
-
+/**
+ * @author Lars Helge Overland
+ * @version $Id$
+ */
 public class PatientStoreTest
     extends DhisSpringTest
 {
@@ -29,7 +63,7 @@ public class PatientStoreTest
         patient.setFirstName( "FirstName" + uniqueChar );
         patient.setMiddleName( "MiddleName" + uniqueChar );
         patient.setLastName( "LastName" + uniqueChar );
-        patient.setGender( "Male" );
+        patient.setGender( Patient.MALE );
         patient.setBirthDate( getDate( 1970, 1, 1 ) );
         
         return patient;
@@ -43,5 +77,34 @@ public class PatientStoreTest
         
         assertEquals( patientA.getFirstName(), patientStore.getPatient( idA ).getFirstName() );
         assertEquals( patientB.getFirstName(), patientStore.getPatient( idB ).getFirstName() );        
+    }
+    
+    @Test
+    public void delete()
+    {
+        int idA = patientStore.addPatient( patientA );
+        int idB = patientStore.addPatient( patientB );
+        
+        assertNotNull( patientStore.getPatient( idA ) );
+        assertNotNull( patientStore.getPatient( idB ) );
+
+        patientStore.deletePatient( patientA );
+        
+        assertNull( patientStore.getPatient( idA ) );
+        assertNotNull( patientStore.getPatient( idB ) );
+
+        patientStore.deletePatient( patientB );
+        
+        assertNull( patientStore.getPatient( idA ) );
+        assertNull( patientStore.getPatient( idB ) );        
+    }
+    
+    @Test
+    public void getAll()
+    {
+        patientStore.addPatient( patientA );
+        patientStore.addPatient( patientB );
+        
+        assertTrue( equals( patientStore.getAllPatients(), patientA, patientB ) );
     }
 }
