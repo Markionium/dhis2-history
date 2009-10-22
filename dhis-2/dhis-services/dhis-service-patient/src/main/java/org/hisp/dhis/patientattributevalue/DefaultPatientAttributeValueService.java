c371edbaad49e0567dcccc08f06d7c78b9600fc3
abyot <abyota@gmail.com>
2009-10-22 09:41:39 +0200

    attribute object, interface, implementation and GUI

---
+++ dhis-2/dhis-services/dhis-service-patient/src/main/java/org/hisp/dhis/patientattributevalue/DefaultPatientAttributeValueService.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2004-2009, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of the HISP project nor the names of its contributors may
+ *   be used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.hisp.dhis.patientattributevalue;
+
+import java.util.Collection;
+
+import org.hisp.dhis.patient.Patient;
+import org.hisp.dhis.patient.PatientAttribute;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author Abyot Asalefew
+ * @version $Id$
+ */
+@Transactional
+public class DefaultPatientAttributeValueService
+    implements PatientAttributeValueService
+{
+
+    // -------------------------------------------------------------------------
+    // Dependencies
+    // -------------------------------------------------------------------------
+
+    private PatientAttributeValueStore patientAttributeValueStore;
+
+    public void setPatientAttributeValueStore( PatientAttributeValueStore patientAttributeValueStore )
+    {
+        this.patientAttributeValueStore = patientAttributeValueStore;
+    }
+
+    // -------------------------------------------------------------------------
+    // PatientAttributeValue
+    // -------------------------------------------------------------------------
+
+    public void deletePatientAttributeValue( PatientAttributeValue patientAttributeValue )
+    {
+        patientAttributeValueStore.delete( patientAttributeValue );
+    }
+
+    public int deletePatientAttributeValue( Patient patient )
+    {
+        return patientAttributeValueStore.deleteByPatient( patient );
+    }
+
+    public int deletePatientAttributeValue( PatientAttribute patientAttribute )
+    {
+        return patientAttributeValueStore.deleteByAttribute( patientAttribute );
+    }
+
+    public Collection<PatientAttributeValue> getAllPatientAttributeValues()
+    {
+        return patientAttributeValueStore.getAll();
+    }
+
+    public PatientAttributeValue getPatientAttributeValue( Patient patient, PatientAttribute patientAttribute )
+    {
+        return patientAttributeValueStore.get( patient, patientAttribute );
+    }
+
+    public Collection<PatientAttributeValue> getPatientAttributeValues( Patient patient )
+    {
+        return patientAttributeValueStore.get( patient );
+    }
+
+    public Collection<PatientAttributeValue> getPatientAttributeValues( PatientAttribute patientAttribute )
+    {
+        return patientAttributeValueStore.get( patientAttribute );
+    }
+
+    public Collection<PatientAttributeValue> getPatientAttributeValues( Collection<Patient> patients )
+    {
+        return patientAttributeValueStore.get( patients );
+    }
+
+    public void savePatientAttributeValue( PatientAttributeValue patientAttributeValue )
+    {
+        if ( patientAttributeValue.getValue() != null )
+        {            
+            patientAttributeValueStore.saveVoid( patientAttributeValue );
+        }       
+    }
+
+    public void updatePatientAttributeValue( PatientAttributeValue patientAttributeValue )
+    {
+        patientAttributeValueStore.update( patientAttributeValue );
+    }
+}
