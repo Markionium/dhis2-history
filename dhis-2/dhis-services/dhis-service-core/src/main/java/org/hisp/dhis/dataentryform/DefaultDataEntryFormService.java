d4e053a658bd5aa14dd46799fbe3446e8b7af66f
Lars Helge Overland <larshelge@gmail.com>
2011-05-08 15:03:02 +0200

    m

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/dataentryform/DefaultDataEntryFormService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/dataentryform/DefaultDataEntryFormService.java
@@ -237,7 +237,6 @@ public class DefaultDataEntryFormService
                     }
                 }
 
-                //inputHtml += "/>";
                 inputMatcher.appendReplacement( sb, inputHtml );
             }
         }
