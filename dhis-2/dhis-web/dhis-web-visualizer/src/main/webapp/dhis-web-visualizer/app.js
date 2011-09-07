c2737ea6bd177fa256d694b5341496ef504d9160
Jan Henrik Overland <janhenrik.overland@gmail.com>
2011-09-07 15:29:05 +0200

    Visualizer.

--- dhis-2/dhis-web/dhis-web-visualizer/src/main/webapp/dhis-web-visualizer/app.js
+++ dhis-2/dhis-web/dhis-web-visualizer/src/main/webapp/dhis-web-visualizer/app.js
@@ -1,26 +1,24 @@
 Ext.onReady( function() {
     
-    Ext.define('simple', {
-        extend: 'Ext.data.Model',
-        fields: ['id', 'name']
-    });
+    DV = {};
+    
+    DV.conf = {
+        finals: {
+            dimension_indicator: 'indicator',
+            dimension_dataelement: 'dataelement',
+            dimension_period: 'period',
+            dimension_organisationunit: 'organisationunit'
+        }
+    };
         
-    DV = {
-        conf: {
-            finals: {
-                dimension_indicator: 'indicator',
-                dimension_dataelement: 'dataelement',
-                dimension_period: 'period',
-                dimension_organisationunit: 'organisationunit'
-            }
-        },
+    DV.app = {
         
         util: {
             getCmp: function(q) {
-                return DV.viewport.query(q)[0];
+                return DV.app.viewport.query(q)[0];
             },
             getViewportSize: function() {
-                var c = DV.getCmp('panel[region="center"]');
+                var c = DV.app.getCmp('panel[region="center"]');
                 return { x: c.getWidth(), y: c.getHeight() };
             }
         },
@@ -64,8 +62,8 @@ Ext.onReady( function() {
             chart: null,        
             getChart: function() {
                 this.chart = Ext.create('Ext.chart.Chart', {
-                    width: DV.util.getViewportSize.x,
-                    height: DV.util.getViewportSize.y,
+                    width: DV.app.util.getViewportSize.x,
+                    height: DV.app.util.getViewportSize.y,
                     animate: true,
                     store: this.store,
                     theme: 'Green',
@@ -105,7 +103,7 @@ Ext.onReady( function() {
             },
             
             reload: function() {
-                var c = DV.util.getCmp('panel[region="center"]');
+                var c = DV.app.util.getCmp('panel[region="center"]');
                 c.removeAll(true);
                 c.add(this.chart);
             }
@@ -130,7 +128,7 @@ Ext.onReady( function() {
                             text: 'Chart settings',
                             style: 'font-weight:bold; padding:0 5px'
                         },
-                        '',
+                        ' ',
                         {
                             xtype: 'button',
                             text: 'Column',
@@ -162,16 +160,18 @@ Ext.onReady( function() {
                                 }
                             }
                         },
-                        '',
+                        ' ',' ',
                         {
                             xtype: 'combobox',
+                            name: 'series',
                             emptyText: 'Series',
                             queryMode: 'local',
+                            editable: false,
                             valueField: 'id',
                             displayField: 'name',
                             width: 90,
                             store: Ext.create('Ext.data.Store', {
-                                model: 'simple',
+                                fields: ['id', 'name'],
                                 data: [
                                     { id: 'indicator', name: 'Indicator' },
                                     { id: 'dataelement', name: 'Data element' },
@@ -180,15 +180,18 @@ Ext.onReady( function() {
                                 ]
                             })
                         },
+                        ' ',
                         {
                             xtype: 'combobox',
+                            name: 'columns',
                             emptyText: 'Columns',
                             queryMode: 'local',
+                            editable: false,
                             valueField: 'id',
                             displayField: 'name',
                             width: 90,
                             store: Ext.create('Ext.data.Store', {
-                                model: 'simple',
+                                fields: ['id', 'name'],
                                 data: [
                                     { id: 'indicator', name: 'Indicator' },
                                     { id: 'dataelement', name: 'Data element' },
@@ -197,15 +200,18 @@ Ext.onReady( function() {
                                 ]
                             })
                         },
+                        ' ',
                         {
                             xtype: 'combobox',
+                            name: 'filter',
                             emptyText: 'Filter',
                             queryMode: 'local',
+                            editable: false,
                             valueField: 'id',
                             displayField: 'name',
                             width: 90,
                             store: Ext.create('Ext.data.Store', {
-                                model: 'simple',
+                                fields: ['id', 'name'],
                                 data: [
                                     { id: 'indicator', name: 'Indicator' },
                                     { id: 'dataelement', name: 'Data element' },
@@ -216,189 +222,44 @@ Ext.onReady( function() {
                         }
                     ],
                     items: [
-                        {
-                            xtype: 'panel',
-                            layout: 'column',
-                            bodyStyle: 'border-style:none; padding-bottom:5px',
-                            items: [
-                                {
-                                    xtype: 'label',
-                                    text: 'Series:',
-                                    width: 50,
-                                    style: 'line-height:21px;'
-                                },
-                                {
-                                    xtype: 'button',
-                                    style: 'margin-left:10px;',
-                                    text: 'Indicator',
-                                    toggleGroup: 'series',
-                                    pressed: true,
-                                    handler: function(b) {
-                                        if (!b.pressed) {
-                                            b.toggle();
-                                        }
-                                    }
-                                },
-                                {
-                                    xtype: 'button',
-                                    style: 'margin-left:5px;',
-                                    text: 'Data element',
-                                    toggleGroup: 'series',
-                                    handler: function(b) {
-                                        if (!b.pressed) {
-                                            b.toggle();
-                                        }
-                                    }
-                                },
-                                {
-                                    xtype: 'button',
-                                    style: 'margin-left:5px;',
-                                    text: 'Period',
-                                    toggleGroup: 'series',
-                                    handler: function(b) {
-                                        if (!b.pressed) {
-                                            b.toggle();
-                                        }
-                                    }
-                                },
-                                {
-                                    xtype: 'button',
-                                    style: 'margin-left:5px;',
-                                    text: 'Organisation unit',
-                                    toggleGroup: 'series',
-                                    handler: function(b) {
-                                        if (!b.pressed) {
-                                            b.toggle();
-                                        }
-                                    }
-                                }
-                            ]
-                        },
-                        {
-                            xtype: 'panel',
-                            layout: 'column',
-                            bodyStyle: 'border-style:none; padding-bottom:5px',
-                            items: [
-                                {
-                                    xtype: 'label',
-                                    text: 'Column:',
-                                    width: 50,
-                                    style: 'line-height:21px;'
-                                },
-                                {
-                                    xtype: 'button',
-                                    style: 'margin-left:10px;',
-                                    text: 'Indicator',
-                                    toggleGroup: 'column',
-                                    pressed: true,
-                                    handler: function(b) {
-                                        if (!b.pressed) {
-                                            b.toggle();
-                                        }
-                                    }
-                                },
-                                {
-                                    xtype: 'button',
-                                    style: 'margin-left:5px;',
-                                    text: 'Data element',
-                                    toggleGroup: 'column',
-                                    handler: function(b) {
-                                        if (!b.pressed) {
-                                            b.toggle();
-                                        }
-                                    }
-                                },
-                                {
-                                    xtype: 'button',
-                                    style: 'margin-left:5px;',
-                                    text: 'Period',
-                                    toggleGroup: 'column',
-                                    handler: function(b) {
-                                        if (!b.pressed) {
-                                            b.toggle();
-                                        }
-                                    }
-                                },
-                                {
-                                    xtype: 'button',
-                                    style: 'margin-left:5px;',
-                                    text: 'Organisation unit',
-                                    toggleGroup: 'column',
-                                    handler: function(b) {
-                                        if (!b.pressed) {
-                                            b.toggle();
-                                        }
-                                    }
-                                }
-                            ]
-                        },
-                        {
-                            xtype: 'panel',
-                            layout: 'column',
-                            bodyStyle: 'border-style:none;',
-                            items: [
-                                {
-                                    xtype: 'label',
-                                    text: 'Filter:',
-                                    width: 50,
-                                    style: 'line-height:21px;'
-                                },
-                                {
-                                    xtype: 'button',
-                                    style: 'margin-left:10px;',
-                                    text: 'Indicator',
-                                    toggleGroup: 'filter',
-                                    pressed: true,
-                                    handler: function(b) {
-                                        if (!b.pressed) {
-                                            b.toggle();
-                                        }
-                                    }
-                                },
-                                {
-                                    xtype: 'button',
-                                    style: 'margin-left:5px;',
-                                    text: 'Data element',
-                                    toggleGroup: 'filter',
-                                    handler: function(b) {
-                                        if (!b.pressed) {
-                                            b.toggle();
-                                        }
-                                    }
-                                },
-                                {
-                                    xtype: 'button',
-                                    style: 'margin-left:5px;',
-                                    text: 'Period',
-                                    toggleGroup: 'filter',
-                                    handler: function(b) {
-                                        if (!b.pressed) {
-                                            b.toggle();
-                                        }
-                                    }
-                                },
-                                {
-                                    xtype: 'button',
-                                    style: 'margin-left:5px;',
-                                    text: 'Organisation unit',
-                                    toggleGroup: 'filter',
-                                    handler: function(b) {
-                                        if (!b.pressed) {
-                                            b.toggle();
-                                        }
-                                    }
-                                }
-                            ]
-                        }
+                        //{
+                            //xtype: 'fieldset',
+                            //title: 'Indicator',
+                            //collapsible: true,
+                            //layout: 'form',
+                            //items: [
+                                //{
+                                    //xtype: 'combobox',
+                                    //name: 'indicatorgroup',
+                                    //fieldLabel: 'Indicator group',
+                                    //queryMode: 'remote',
+                                    //editable: false,
+                                    //valueField: 'id',
+                                    //displayField: 'name',
+                                    //store: Ext.create('Ext.data.Store', {
+                                        //fields: ['id', 'name'],
+                                        //proxy: {
+                                            //type: 'ajax',
+                                            //url: '
+                                    
+                                //{
+                                    //xtype: 'itemselector',
+                                    //name: 'indicator',
+                                    
+                    
+                            //]
+                        //}
+                    
+                    
                     ],
                     listeners: {
                         collapse: function(p) {                    
                             p.collapsed = true;
-                            DV.util.getCmp('button[name="resize"]').setText('>>');
+                            DV.app.util.getCmp('button[name="resize"]').setText('>>');
                         },
                         expand: function(p) {
                             p.collapsed = false;
-                            DV.util.getCmp('button[name="resize"]').setText('<<');
+                            DV.app.util.getCmp('button[name="resize"]').setText('<<');
                         }
                     }
                 },
@@ -413,7 +274,7 @@ Ext.onReady( function() {
                             text: '<<',
                             toolTip: 'Collapse',
                             handler: function() {
-                                var p = DV.util.getCmp('panel[region="west"]');
+                                var p = DV.app.util.getCmp('panel[region="west"]');
                                 if (p.collapsed) {
                                     p.expand();
                                 }
@@ -431,26 +292,26 @@ Ext.onReady( function() {
                             xtype: 'button',
                             text: 'Load..',
                             handler: function() {
-                                DV.chart.getData();                                
-                                DV.chart.getStore();                                
-                                DV.chart.getChart();                                
-                                DV.chart.reload();
+                                DV.app.chart.getData();                                
+                                DV.app.chart.getStore();                                
+                                DV.app.chart.getChart();                                
+                                DV.app.chart.reload();
                             }
                         },
                         {
                             xtype: 'button',
                             text: 'Reload..',
                             handler: function() {
-                                DV.chart.data = [
+                                DV.app.chart.data = [
                                     { x: 'August 2010', 'anc 1': 17, anc2: 5, anc3: 11, anc4: 14 },
                                     { x: 'September 2010', 'anc 1': 5, anc2: 13, anc3: 16, anc4: 5 },
                                     { x: 'October 2010', 'anc 1': 21, anc2: 6, anc3: 11, anc4: 16 },
                                     { x: 'November 2010', 'anc 1': 15, anc2: 22, anc3: 44, anc4: 5 }
                                 ];
                                 
-                                DV.chart.getStore();                                
-                                DV.chart.getChart();                                
-                                DV.chart.reload();
+                                DV.app.chart.getStore();                                
+                                DV.app.chart.getChart();                                
+                                DV.app.chart.reload();
                             }
                         },
                         '->',
@@ -472,14 +333,14 @@ Ext.onReady( function() {
         })
     };
     
-    //DV.conf.data = [
-        //{ id: DV.conf.finals.dimension_indicator, name: 'Indicator' },
-        //{ id: DV.conf.finals.dimension_dataelement, name: 'Data element' },
-        //{ id: DV.conf.finals.dimension_period, name: 'Period' },
-        //{ id: DV.conf.finals.dimension_organisationunit, name: 'Organisation unit' }
+    //DV.app.conf.data = [
+        //{ id: DV.app.conf.finals.dimension_indicator, name: 'Indicator' },
+        //{ id: DV.app.conf.finals.dimension_dataelement, name: 'Data element' },
+        //{ id: DV.app.conf.finals.dimension_period, name: 'Period' },
+        //{ id: DV.app.conf.finals.dimension_organisationunit, name: 'Organisation unit' }
     //];
     
-    //DV.util.getCmp('combobox').store.loadData(DV.conf.data);
+    //DV.app.util.getCmp('combobox').store.loadData(DV.app.conf.data);
 });
 
 
