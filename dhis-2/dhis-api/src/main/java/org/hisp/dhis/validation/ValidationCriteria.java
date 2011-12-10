d06875ef020f6c95ca338b77f4b515d84736b940
Morten Olav Hansen <mortenoh@gmail.com>
2011-12-10 23:36:01 +0100

    removed uid from validationcriteria.. seems to only be used in case and patient?? also more adapters..

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/validation/ValidationCriteria.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/validation/ValidationCriteria.java
@@ -27,20 +27,11 @@
 
 package org.hisp.dhis.validation;
 
-import org.hisp.dhis.common.BaseIdentifiableObject;
-import org.hisp.dhis.common.Dxf2Namespace;
-
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
-
 /**
  * @author Lars Helge Overland
+ * @version $Id$
  */
-@XmlRootElement( name = "validationCriteria", namespace = Dxf2Namespace.NAMESPACE )
-@XmlAccessorType( value = XmlAccessType.NONE )
 public class ValidationCriteria
-    extends BaseIdentifiableObject
 {
     public static final int OPERATOR_LESS_THAN = -1;
 
@@ -51,6 +42,10 @@ public class ValidationCriteria
     // -------------------------------------------------------------------------
     // Fields
     // -------------------------------------------------------------------------
+
+    private int id;
+
+    private String name;
 
     private String description;
 
@@ -99,6 +94,26 @@ public class ValidationCriteria
     // -------------------------------------------------------------------------
     // Getters && Setters
     // -------------------------------------------------------------------------
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+
+    public void setId( int id )
+    {
+        this.id = id;
+    }
 
     public String getDescription()
     {
