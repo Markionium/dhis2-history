158086eeb1b1065f8195994d6795bee53bb4d59e
Lars Helge Overland <larshelge@gmail.com>
2011-12-15 17:24:21 +0100

    m

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/api/view/MapGenerationView.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/api/view/MapGenerationView.java
@@ -43,7 +43,7 @@ public class MapGenerationView
     extends AbstractView
 {
     @Autowired
-    MapGenerationService mapGenerationService;
+    private MapGenerationService mapGenerationService;
 
     public MapGenerationView()
     {
