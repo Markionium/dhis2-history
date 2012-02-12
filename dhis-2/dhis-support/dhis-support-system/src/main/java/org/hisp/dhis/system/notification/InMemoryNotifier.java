ca15838889a059958b762eac3db9cefdd55e3268
Lars Helge Overland <larshelge@gmail.com>
2012-02-12 14:16:57 +0100

    m

---
+++ dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/system/notification/InMemoryNotifier.java
@@ -0,0 +1,66 @@
+package org.hisp.dhis.system.notification;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+public class InMemoryNotifier
+    implements Notifier
+{
+    private int MAX_SIZE = 2000;
+    
+    private List<Notification> notifications;
+    
+    @PostConstruct
+    public void init()
+    {
+        notifications = new ArrayList<Notification>();
+    }
+
+    // -------------------------------------------------------------------------
+    // Notifier implementation
+    // -------------------------------------------------------------------------
+
+    @Override
+    public void notify( NotificationCategory category, String message )
+    {
+        notify( NotificationLevel.INFO, category, message );
+    }
+    
+    @Override
+    public void notify( NotificationLevel level, NotificationCategory category, String message )
+    {
+        Notification notification = new Notification( level, category, new Date(), message );
+        
+        notifications.add( notification );
+        
+        if ( notifications.size() > MAX_SIZE )
+        {
+            notifications.remove( MAX_SIZE );
+        }
+    }
+
+    @Override
+    public List<Notification> getNotifications()
+    {
+        return notifications;
+    }
+
+    @Override
+    public List<Notification> getNotifications( NotificationCategory category )
+    {
+        List<Notification> list = new ArrayList<Notification>();
+        
+        for ( Notification notification : notifications )
+        {
+            if ( category.equals( notification.getCategory() ) )
+            {
+                list.add( notification );
+            }
+        }
+        
+        return list;
+    }
+}
