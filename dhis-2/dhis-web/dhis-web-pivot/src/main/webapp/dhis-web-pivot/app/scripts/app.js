c1ca59764e6f3c6daa932765c008446d2c60d6ae
Jan Henrik Overland <janhenrik.overland@gmail.com>
2013-01-16 11:28:24 +0100

    pt gui

--- dhis-2/dhis-web/dhis-web-pivot/src/main/webapp/dhis-web-pivot/app/scripts/app.js
+++ dhis-2/dhis-web/dhis-web-pivot/src/main/webapp/dhis-web-pivot/app/scripts/app.js
@@ -31,7 +31,10 @@ Ext.onReady( function() {
 				dataSetAvailable,
 				dataSetSelected,
 				dataSet,
+				rewind,
 				relativePeriod,
+				fixedPeriodAvailable,
+				fixedPeriodSelected,
 				fixedPeriod,
 				organisationUnit
 
@@ -485,10 +488,16 @@ Ext.onReady( function() {
 				}
 			});
 
-			relativePeriod
+			rewind = Ext.create('Ext.form.field.Checkbox', {
+				paramName: 'rewind',
+				boxLabel: 'Rewind one period',
+				xable: function() {
+					this.setDisabled(pt.util.checkbox.isAllFalse());
+				}
+			};
 
-			{
-				title: '<div style="height:17px; background-image:url(images/period.png); background-repeat:no-repeat; padding-left:20px">' + DV.i18n.relative_periods + '</div>',
+			relativePeriod = Ext.create('Ext.panel.Panel', {
+				title: '<div class="pt-panel-title-period">Relative periods</div>', //i18n DV.i18n.relative_periods
 				hideCollapseTool: true,
 				autoScroll: true,
 				items: [
@@ -506,34 +515,34 @@ Ext.onReady( function() {
 									listeners: {
 										added: function(chb) {
 											if (chb.xtype === 'checkbox') {
-												DV.cmp.dimension.relativeperiod.checkbox.push(chb);
+												pt.cmp.dimension.relativeperiod.checkbox.push(chb);
 											}
 										},
 										change: function() {
-											DV.cmp.dimension.relativeperiod.rewind.xable();
+											rewind.xable();
 										}
 									}
 								},
 								items: [
 									{
 										xtype: 'label',
-										text: DV.i18n.months,
-										cls: 'dv-label-period-heading'
+										text: 'Months', //i18n DV.i18n.months,
+										cls: 'pt-label-period-heading'
 									},
 									{
 										xtype: 'checkbox',
 										paramName: 'reportingMonth',
-										boxLabel: DV.i18n.last_month
+										boxLabel: 'Last month', //i18n DV.i18n.last_month
 									},
 									{
 										xtype: 'checkbox',
 										paramName: 'last3Months',
-										boxLabel: DV.i18n.last_3_months
+										boxLabel: 'Last 3 months', //i18n DV.i18n.last_3_months
 									},
 									{
 										xtype: 'checkbox',
 										paramName: 'last12Months',
-										boxLabel: DV.i18n.last_12_months,
+										boxLabel: 'Last 12 months', //i18n DV.i18n.last_12_months,
 										checked: true
 									}
 								]
@@ -547,29 +556,29 @@ Ext.onReady( function() {
 									listeners: {
 										added: function(chb) {
 											if (chb.xtype === 'checkbox') {
-												DV.cmp.dimension.relativeperiod.checkbox.push(chb);
+												pt.cmp.dimension.relativeperiod.checkbox.push(chb);
 											}
 										},
 										change: function() {
-											DV.cmp.dimension.relativeperiod.rewind.xable();
+											rewind.xable();
 										}
 									}
 								},
 								items: [
 									{
 										xtype: 'label',
-										text: DV.i18n.quarters,
+										text: 'Quarters', //i18n DV.i18n.quarters,
 										cls: 'dv-label-period-heading'
 									},
 									{
 										xtype: 'checkbox',
 										paramName: 'reportingQuarter',
-										boxLabel: DV.i18n.last_quarter
+										boxLabel: 'Last quarter', //i18n DV.i18n.last_quarter
 									},
 									{
 										xtype: 'checkbox',
 										paramName: 'last4Quarters',
-										boxLabel: DV.i18n.last_4_quarters
+										boxLabel: 'Last 4 quarters', //i18n DV.i18n.last_4_quarters
 									}
 								]
 							},
@@ -582,29 +591,29 @@ Ext.onReady( function() {
 									listeners: {
 										added: function(chb) {
 											if (chb.xtype === 'checkbox') {
-												DV.cmp.dimension.relativeperiod.checkbox.push(chb);
+												pt.cmp.dimension.relativeperiod.checkbox.push(chb);
 											}
 										},
 										change: function() {
-											DV.cmp.dimension.relativeperiod.rewind.xable();
+											rewind.xable();
 										}
 									}
 								},
 								items: [
 									{
 										xtype: 'label',
-										text: DV.i18n.six_months,
+										text: 'Six-months', //i18n DV.i18n.six_months,
 										cls: 'dv-label-period-heading'
 									},
 									{
 										xtype: 'checkbox',
 										paramName: 'lastSixMonth',
-										boxLabel: DV.i18n.last_six_month
+										boxLabel: 'Last six-month', //i18n DV.i18n.last_six_month
 									},
 									{
 										xtype: 'checkbox',
 										paramName: 'last2SixMonths',
-										boxLabel: DV.i18n.last_two_six_month
+										boxLabel: 'Last two six-months', //i18n DV.i18n.last_two_six_month
 									}
 								]
 							}
@@ -624,34 +633,34 @@ Ext.onReady( function() {
 									listeners: {
 										added: function(chb) {
 											if (chb.xtype === 'checkbox') {
-												DV.cmp.dimension.relativeperiod.checkbox.push(chb);
+												pt.cmp.dimension.relativeperiod.checkbox.push(chb);
 											}
 										},
 										change: function() {
-											DV.cmp.dimension.relativeperiod.rewind.xable();
+											rewind.xable();
 										}
 									}
 								},
 								items: [
 									{
 										xtype: 'label',
-										text: DV.i18n.years,
+										text: 'Years', //i18n DV.i18n.years,
 										cls: 'dv-label-period-heading'
 									},
 									{
 										xtype: 'checkbox',
 										paramName: 'thisYear',
-										boxLabel: DV.i18n.this_year
+										boxLabel: 'This year', //i18n DV.i18n.this_year
 									},
 									{
 										xtype: 'checkbox',
 										paramName: 'lastYear',
-										boxLabel: DV.i18n.last_year
+										boxLabel: 'Last year', //i18n DV.i18n.last_year
 									},
 									{
 										xtype: 'checkbox',
 										paramName: 'last5Years',
-										boxLabel: DV.i18n.last_5_years
+										boxLabel: 'Last 5 years', //i18n DV.i18n.last_5_years
 									}
 								]
 							},
@@ -666,21 +675,9 @@ Ext.onReady( function() {
 									{
 										xtype: 'label',
 										text: 'Options',
-										cls: 'dv-label-period-heading-options'
+										cls: 'pt-label-period-heading-options'
 									},
-									{
-										xtype: 'checkbox',
-										paramName: 'rewind',
-										boxLabel: 'Rewind one period',
-										xable: function() {
-											this.setDisabled(DV.util.checkbox.isAllFalse());
-										},
-										listeners: {
-											added: function() {
-												DV.cmp.dimension.relativeperiod.rewind = this;
-											}
-										}
-									}
+									rewind
 								]
 							}
 						]
@@ -688,13 +685,187 @@ Ext.onReady( function() {
 				],
 				listeners: {
 					added: function() {
-						DV.cmp.dimension.relativeperiod.panel = this;
+						pt.cmp.dimension.panels.push(this);
 					},
 					expand: function() {
-						DV.util.dimension.panel.setHeight(DV.conf.layout.west_maxheight_accordion_relativeperiod);
+						pt.util.dimension.panel.setHeight(pt.conf.layout.west_maxheight_accordion_relativeperiod);
 					}
 				}
-			},
+			});
+
+			fixedPeriodAvailable = Ext.create('Ext.ux.form.MultiSelect', {
+				cls: 'pt-toolbar-multiselect-left',
+				width: (pt.conf.layout.west_fieldset_width - pt.conf.layout.west_width_padding) / 2,
+				valueField: 'id',
+				displayField: 'name',
+				store: DV.store.fixedPeriodAvailable,
+				tbar: [
+					{
+						xtype: 'label',
+						text: 'Available', //i18n DV.i18n.available,
+						cls: 'pt-toolbar-multiselect-left-label'
+					},
+					'->',
+					{
+						xtype: 'button',
+						icon: 'images/arrowright.png',
+						width: 22,
+						handler: function() {
+							pt.util.multiselect.select(fixedPeriodAvailable, fixedPeriodSelected);
+						}
+					},
+					{
+						xtype: 'button',
+						icon: 'images/arrowrightdouble.png',
+						width: 22,
+						handler: function() {
+							pt.util.multiselect.selectAll(fixedPeriodAvailable, fixedPeriodSelected);
+						}
+					},
+					' '
+				],
+				listeners: {
+					afterrender: function() {
+						this.boundList.on('itemdblclick', function() {
+							pt.util.multiselect.select(fixedPeriodAvailable, fixedPeriodSelected);
+						}, this);
+					}
+				}
+			});
+
+			fixedPeriodSelected = Ext.create('Ext.ux.form.MultiSelect', {
+				cls: 'dv-toolbar-multiselect-right',
+				width: (pt.conf.layout.west_fieldset_width - pt.conf.layout.west_width_padding) / 2,
+				valueField: 'id',
+				displayField: 'name',
+				ddReorder: false,
+				store: DV.store.fixedPeriodSelected,
+				tbar: [
+					' ',
+					{
+						xtype: 'button',
+						icon: 'images/arrowleftdouble.png',
+						width: 22,
+						handler: function() {
+							pt.util.multiselect.unselectAll(fixedPeriodAvailable, fixedPeriodSelected);
+						}
+					},
+					{
+						xtype: 'button',
+						icon: 'images/arrowleft.png',
+						width: 22,
+						handler: function() {
+							pt.util.multiselect.unselect(fixedPeriodAvailable, fixedPeriodSelected);
+						}
+					},
+					'->',
+					{
+						xtype: 'label',
+						text: 'Selected', //i18n DV.i18n.selected,
+						cls: 'dv-toolbar-multiselect-right-label'
+					}
+				],
+				listeners: {
+					afterrender: function() {
+						this.boundList.on('itemdblclick', function() {
+							pt.util.multiselect.unselect(fixedPeriodAvailable, fixedPeriodSelected);
+						}, this);
+					}
+				}
+			});
+
+			fixedPeriods = Ext.create('Ext.panel.Panel', {
+				title: '<div class="pt-panel-title-period">Fixed periods</div>', //i18n DV.i18n.fixed_periods
+				hideCollapseTool: true,
+				items: [
+					{
+						xtype: 'panel',
+						layout: 'column',
+						bodyStyle: 'border-style:none',
+						items: [
+							{
+								xtype: 'combobox',
+								cls: 'pt-combo',
+								style: 'margin-bottom:8px',
+								width: 261,
+								valueField: 'id',
+								displayField: 'name',
+								fieldLabel: 'Select type', //i18n DV.i18n.select_type,
+								fieldCls: 'pt-combo-label-default',
+								editable: false,
+								queryMode: 'remote',
+								store: pt.store.periodType,
+								periodOffset: 0,
+								listeners: {
+									select: function() {
+										var pt = new PeriodType(),
+											periodType = this.getValue();
+
+										var periods = pt.get(periodType).generatePeriods({
+											offset: this.periodOffset,
+											filterFuturePeriods: true,
+											reversePeriods: true
+										});
+
+										pt.store.fixedPeriodAvailable.setIndex(periods);
+										pt.store.fixedPeriodAvailable.loadData(periods);
+										pt.util.multiselect.filterAvailable(fixedPeriodAvailable, fixedPeriodSelected);
+									}
+								}
+							},
+							{
+								xtype: 'button',
+								text: 'Prev year', //i18n
+								style: 'margin-left:4px',
+								height: 24,
+								handler: function() {
+									var cb = this.up('panel').down('combobox');
+									if (cb.getValue()) {
+										cb.periodOffset--;
+										cb.fireEvent('select');
+									}
+								}
+							},
+							{
+								xtype: 'button',
+								text: 'Next year', //i18n
+								style: 'margin-left:3px',
+								height: 24,
+								handler: function() {
+									var cb = this.up('panel').down('combobox');
+									if (cb.getValue() && cb.periodOffset < 0) {
+										cb.periodOffset++;
+										cb.fireEvent('select');
+									}
+								}
+							}
+						]
+					},
+					{
+						xtype: 'panel',
+						layout: 'column',
+						bodyStyle: 'border-style:none',
+						items: [
+							fixedPeriodAvailable,
+							fixedPeriodSelected
+						]
+					}
+				],
+				listeners: {
+					added: function() {
+						pt.cmp.dimension.panels.push(this);
+					},
+					expand: function() {
+						pt.util.dimension.panel.setHeight(pt.conf.layout.west_maxheight_accordion_fixedperiod);
+						pt.util.multiselect.setHeight(
+							[fixedPeriodAvailable, fixedPeriodSelected],
+							this,
+							pt.conf.layout.west_fill_accordion_fixedperiod
+						);
+					}
+				}
+			});
+
 
 
 
