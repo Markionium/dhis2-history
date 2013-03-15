4da83b4f292bee81996d8c90f12e7bb25a84cd1a
Jan Henrik Overland <janhenrik.overland@gmail.com>
2013-03-15 09:29:40 +0100

    dv sharing + fav bug fixed

--- dhis-2/dhis-web/dhis-web-visualizer/src/main/webapp/dhis-web-visualizer/app/scripts/app.js
+++ dhis-2/dhis-web/dhis-web-visualizer/src/main/webapp/dhis-web-visualizer/app/scripts/app.js
@@ -2711,16 +2711,16 @@ Ext.onReady( function() {
 				if (p.lastMonth || p.last3Months || p.last12Months || p.lastQuarter || p.last4Quarters ||
 					p.lastSixMonth || p.last2SixMonths || p.thisYear || p.lastYear || p.last5Years) {
 
-					if (p.lastMonth) {favorite.lastMonth = true;}
-					if (p.last3Months) {favorite.last3Months = true;}
-					if (p.last12Months) {favorite.last12Months = true;}
-					if (p.lastQuarter) {favorite.lastQuarter = true;}
-					if (p.last4Quarters) {favorite.last4Quarters = true;}
-					if (p.lastSixMonth) {favorite.lastSixMonth = true;}
-					if (p.last2SixMonths) {favorite.last2SixMonths = true;}
-					if (p.thisYear) {favorite.thisYear = true;}
-					if (p.lastYear) {favorite.lastYear = true;}
-					if (p.last5Years) {favorite.last5Years = true;}
+					if (p.lastMonth) {favorite.relativePeriods.lastMonth = true;}
+					if (p.last3Months) {favorite.relativePeriods.last3Months = true;}
+					if (p.last12Months) {favorite.relativePeriods.last12Months = true;}
+					if (p.lastQuarter) {favorite.relativePeriods.lastQuarter = true;}
+					if (p.last4Quarters) {favorite.relativePeriods.last4Quarters = true;}
+					if (p.lastSixMonth) {favorite.relativePeriods.lastSixMonth = true;}
+					if (p.last2SixMonths) {favorite.relativePeriods.last2SixMonths = true;}
+					if (p.thisYear) {favorite.relativePeriods.thisYear = true;}
+					if (p.lastYear) {favorite.relativePeriods.lastYear = true;}
+					if (p.last5Years) {favorite.relativePeriods.last5Years = true;}
 				}
 
 				if (p.rewind) {
@@ -3219,6 +3219,265 @@ Ext.onReady( function() {
 		});
 
 		return favoriteWindow;
+	};
+
+	DV.app.SharingWindow = function(sharing) {
+
+		// Objects
+		var UserGroupRow,
+
+		// Functions
+			getBody,
+
+		// Components
+			userGroupStore,
+			userGroupField,
+			userGroupButton,
+			userGroupRowContainer,
+			publicGroup,
+			window;
+
+		UserGroupRow = function(obj, isPublicAccess, disallowPublicAccess) {
+			var getData,
+				store,
+				getItems,
+				combo,
+				getAccess,
+				panel;
+
+			getData = function() {
+				var data = [
+					{id: 'r-------', name: 'Can view'}, //i18n
+					{id: 'rw------', name: 'Can edit and view'}
+				];
+
+				if (isPublicAccess) {
+					data.unshift({id: '-------', name: 'None'});
+				}
+
+				return data;
+			}
+
+			store = Ext.create('Ext.data.Store', {
+				fields: ['id', 'name'],
+				data: getData()
+			});
+
+			getItems = function() {
+				var items = [];
+
+				combo = Ext.create('Ext.form.field.ComboBox', {
+					fieldLabel: isPublicAccess ? 'Public access' : obj.name, //i18n
+					labelStyle: 'color:#333',
+					cls: 'pt-combo',
+					width: 380,
+					labelWidth: 250,
+					queryMode: 'local',
+					valueField: 'id',
+					displayField: 'name',
+					labelSeparator: null,
+					editable: false,
+					disabled: !!disallowPublicAccess,
+					value: obj.access,
+					store: store
+				});
+
+				items.push(combo);
+
+				if (!isPublicAccess) {
+					items.push(Ext.create('Ext.Img', {
+						src: 'images/grid-delete_16.png',
+						style: 'margin-top:2px; margin-left:7px',
+						overCls: 'pointer',
+						width: 16,
+						height: 16,
+						listeners: {
+							render: function(i) {
+								i.getEl().on('click', function(e) {
+									i.up('panel').destroy();
+									window.doLayout();
+								});
+							}
+						}
+					}));
+				}
+
+				return items;
+			};
+
+			getAccess = function() {
+				return {
+					id: obj.id,
+					name: obj.name,
+					access: combo.getValue()
+				};
+			};
+
+			panel = Ext.create('Ext.panel.Panel', {
+				layout: 'column',
+				bodyStyle: 'border:0 none',
+				getAccess: getAccess,
+				items: getItems()
+			});
+
+			return panel;
+		};
+
+		getBody = function() {
+			var body = {
+				object: {
+					id: sharing.object.id,
+					name: sharing.object.name,
+					publicAccess: publicGroup.down('combobox').getValue(),
+					user: {
+						id: DV.init.user.id,
+						name: DV.init.user.name
+					}
+				}
+			};
+
+			if (userGroupRowContainer.items.items.length > 1) {
+				body.object.userGroupAccesses = [];
+				for (var i = 1, item; i < userGroupRowContainer.items.items.length; i++) {
+					item = userGroupRowContainer.items.items[i];
+					body.object.userGroupAccesses.push(item.getAccess());
+				}
+			}
+
+			return body;
+		};
+
+		// Initialize
+		userGroupStore = Ext.create('Ext.data.Store', {
+			fields: ['id', 'name'],
+			proxy: {
+				type: 'ajax',
+				url: DV.init.contextPath + '/api/sharing/search',
+				reader: {
+					type: 'json',
+					root: 'userGroups'
+				}
+			}
+		});
+
+		userGroupField = Ext.create('Ext.form.field.ComboBox', {
+			valueField: 'id',
+			displayField: 'name',
+			emptyText: 'Search for user groups', //i18n
+			queryParam: 'key',
+			queryDelay: 200,
+			minChars: 1,
+			hideTrigger: true,
+			fieldStyle: 'height:26px; padding-left:6px; border-radius:1px; font-size:11px',
+			style: 'margin-bottom:5px',
+			width: 380,
+			store: userGroupStore,
+			listeners: {
+				beforeselect: function(cb) { // beforeselect instead of select, fires regardless of currently selected item
+					userGroupButton.enable();
+				},
+				afterrender: function(cb) {
+					cb.inputEl.on('keyup', function() {
+						userGroupButton.disable();
+					});
+				}
+			}
+		});
+
+		userGroupButton = Ext.create('Ext.button.Button', {
+			text: '+',
+			style: 'margin-left:2px; padding-right:4px; padding-left:4px; border-radius:1px',
+			disabled: true,
+			height: 26,
+			handler: function(b) {
+				userGroupRowContainer.add(UserGroupRow({
+					id: userGroupField.getValue(),
+					name: userGroupField.getRawValue(),
+					access: 'r-------'
+				}));
+
+				userGroupField.clearValue();
+				b.disable();
+			}
+		});
+
+		userGroupRowContainer = Ext.create('Ext.container.Container', {
+			bodyStyle: 'border:0 none'
+		});
+
+		publicGroup = userGroupRowContainer.add(UserGroupRow({
+			id: sharing.object.id,
+			name: sharing.object.name,
+			access: sharing.object.publicAccess
+		}, true, !sharing.meta.allowPublicAccess));
+
+		if (Ext.isArray(sharing.object.userGroupAccesses)) {
+			for (var i = 0, userGroupRow; i < sharing.object.userGroupAccesses.length; i++) {
+				userGroupRow = UserGroupRow(sharing.object.userGroupAccesses[i]);
+				userGroupRowContainer.add(userGroupRow);
+			}
+		}
+
+		window = Ext.create('Ext.window.Window', {
+			title: 'Sharing layout',
+			bodyStyle: 'padding:8px 8px 3px; background-color:#fff',
+			width: 434,
+			resizable: false,
+			modal: true,
+			destroyOnBlur: true,
+			items: [
+				{
+					html: sharing.object.name,
+					bodyStyle: 'border:0 none; font-weight:bold; color:#333',
+					style: 'margin-bottom:8px'
+				},
+				{
+					xtype: 'container',
+					layout: 'column',
+					bodyStyle: 'border:0 none',
+					items: [
+						userGroupField,
+						userGroupButton
+					]
+				},
+				userGroupRowContainer
+			],
+			bbar: [
+				'->',
+				{
+					text: 'Save',
+					handler: function() {
+						Ext.Ajax.request({
+							url: DV.init.contextPath + '/api/sharing?type=chart&id=' + sharing.object.id,
+							method: 'POST',
+							headers: {
+								'Content-Type': 'application/json'
+							},
+							params: Ext.encode(getBody())
+						});
+
+						window.destroy();
+					}
+				}
+			],
+			listeners: {
+				show: function(w) {
+					var pos = DV.cmp.favorite.window.getPosition();
+					w.setPosition(pos[0] + 5, pos[1] + 5);
+
+					//if (!w.hasDestroyOnBlurHandler) {
+						//pt.util.window.addDestroyOnBlurHandler(w);
+					//}
+
+					//pt.viewport.favoriteWindow.destroyOnBlur = false;
+				},
+				destroy: function() {
+					//pt.viewport.favoriteWindow.destroyOnBlur = true;
+				}
+			}
+		});
+
+		return window;
 	};
 
     DV.viewport = Ext.create('Ext.container.Viewport', {
