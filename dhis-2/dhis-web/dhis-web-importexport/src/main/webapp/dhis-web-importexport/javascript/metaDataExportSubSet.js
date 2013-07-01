b32758b32da273ccf132ded2f485cb59fff13510
Rosu Ovidiu rosu.ovi@gmail.com <Rosu Ovidiu rosu.ovi@gmail.com>
2013-07-02 01:32:35 +0300

    ID 12: Major update on Export Module UI.

--- dhis-2/dhis-web/dhis-web-importexport/src/main/webapp/dhis-web-importexport/javascript/metaDataExportSubSet.js
+++ dhis-2/dhis-web/dhis-web-importexport/src/main/webapp/dhis-web-importexport/javascript/metaDataExportSubSet.js
@@ -1,37 +1,15 @@
 function selectAll() {
     $("#exportForm").find("input:checkbox").attr("checked", true);
-}
+};
 
 function selectNone() {
     $("#exportForm").find("input:checkbox").attr("checked", false);
+};
+
+function lowercaseFirstLetter(string) {
+    return string.charAt(0).toLowerCase() + string.slice(1);
 }
 
-function displayMetaData() {
-
-    if ($('#attributeTypes').is(':checked')) {
-        $('#availableAttributes').show();
-    } else {
-        $('#availableAttributes').hide();
-    }
-
-    if ($('#categories').is(':checked')) {
-        $('#availableDataElementCategories').show();
-    } else {
-        $('#availableDataElementCategories').hide();
-    }
-
-    if ($('#dataSets').is(':checked')) {
-        $('#availableDataSets').show();
-    } else {
-        $('#availableDataSets').hide();
-    }
-
-    if ($('#dataElements').is(':checked')) {
-        $('#availableDataElements').show();
-    } else {
-        $('#availableDataElements').hide();
-    }
-}
 
 function exportMetaDataSubSet() {
 
@@ -86,4 +64,77 @@ function exportMetaDataSubSet() {
 
     alert(url);
     window.location = url;
-}
+};
+
+//function sendInformation() {
+//    var data;
+//    if ($('#attributeTypes').is(':checked')) {
+//        data = {dataElementAttribute: "false"};
+//    }
+//    $.ajax(
+//        {
+//            url: "../api/metaData.xml.zip",
+//            type: "GET",
+//            data: data,
+//            dataType: "text",
+//            success: function () {
+//                alert("A MERS OVIDIU");
+//            },
+//            error: function (data, status, er) {
+//                alert("error: " + data + " status: " + status + " er:" + er);
+//            }
+//        }
+//    )
+//};
+
+// Collapse Metadata Category information
+jQuery(function () {
+    $('#attributeTypes').click(function () {
+        if ($('#attributeTypes').is(':checked')) {
+            $('#attributeTypesSelectionArea').show();
+            loadMetadata("Attributes");
+        } else {
+            $('#attributeTypesSelectionArea').show();
+        }
+    });
+
+    $('#dataSets').click(function () {
+        if ($('#dataSets').is(':checked')) {
+            $('#dataSetsSelectionArea').show();
+            loadMetadata("DataSets");
+        } else {
+            $('#dataSetsSelectionArea').show();
+        }
+    })
+});
+
+// Deselect all Metadata categories combo boxes
+jQuery(function () {
+    selectNone();
+});
+
+// Load MetaData for a Category
+function loadMetadata(metadataCategoryName) {
+    jQuery("#available" + metadataCategoryName).dhisAjaxSelect({
+        source: "../dhis-web-commons-ajax-json/get" + metadataCategoryName + ".action",
+        iterator: lowercaseFirstLetter(metadataCategoryName),
+        connectedTo: "selected" + metadataCategoryName,
+        handler: function (item) {
+            var option = jQuery("<option data-id='" + item.groups + "' />");
+            option.text(item.name);
+            option.attr("value", item.id);
+
+            return option;
+        }
+    });
+};
+
+
+
+
+
+
+
+
+
+
