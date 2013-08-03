8d46dd3594993d6ff35ccf83f3f76ec85a9ac67b
Jan Henrik Overland <janhenrik.overland@gmail.com>
2013-08-03 12:55:27 +0300

    dv plugin wip

--- dhis-2/dhis-web/dhis-web-visualizer/src/main/webapp/dhis-web-visualizer/app/scripts/plugin.js
+++ dhis-2/dhis-web/dhis-web-visualizer/src/main/webapp/dhis-web-visualizer/app/scripts/plugin.js
@@ -9,22 +9,39 @@ Ext.onReady(function() {
 	};
 	
 	DV.plugin.getChart = function(config) {
-		var afterRender,
+		var validateConfig,
+			afterRender,
 			createViewport,
 			dv,
 			initialize;
 			
-		afterRender = function() {
+		validateConfig = function(config) {
+			if (!Ext.isObject(config)) {
+				console.log('Invalid chart configuration');
+				return;
+			}
 			
+			if (!Ext.isString(config.el)) {
+				console.log('No element provided');
+				return;
+			}
+			
+			if (!Ext.isString(config.uid)) {
+				console.log('No chart uid provided');
+				return;
+			}
+			
+			return true;
 		};
 			
+		afterRender = function() {};
+			
 		createViewport = function() {
-			var setFavorite,
+			var el = Ext.get(dv.el),
+				setFavorite,
 				centerRegion,
-				el = Ext.get(dv.el),
 				width,
-				height,
-				viewportConfig;
+				height;
 				
 			width = el.getWidth() - parseInt(el.getStyle('border-left-width')) - parseInt(el.getStyle('border-right-width'));
 			height = el.getHeight() - parseInt(el.getStyle('border-top-width')) - parseInt(el.getStyle('border-bottom-width'));				
@@ -53,25 +70,23 @@ Ext.onReady(function() {
 		};
 		
 		initialize = function() {
+			if (!validateConfig(config)) {
+				return;
+			}
 			
 			dv = DV.core.getInstance({
 				baseUrl: config.url,
 				el: config.el
 			});
+			
+			dv.viewport = createViewport();
 
 			Ext.data.JsonP.request({
 				url: dv.baseUrl + '/dhis-web-visualizer/initialize.action',
 				success: function(r) {
 					dv.init = r;
-			
-					dv.viewport = createViewport();
 					
-					if (Ext.isString(config.uid)) {
-						dv.util.chart.loadChart(config.uid, true);
-						return;
-					}
-					
-					console.log("nothing to do");					
+					dv.util.chart.loadChart(config.uid, true);	
 				}
 			});
 		}();
