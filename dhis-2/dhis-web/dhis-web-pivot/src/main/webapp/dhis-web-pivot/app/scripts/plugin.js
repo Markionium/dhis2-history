c243d046c84b343c72e4999af0edf5e588237a81
Jan Henrik Overland <janhenrik.overland@gmail.com>
2013-08-04 17:34:28 +0300

    pt app/plugin restructure wip

--- dhis-2/dhis-web/dhis-web-pivot/src/main/webapp/dhis-web-pivot/app/scripts/plugin.js
+++ dhis-2/dhis-web/dhis-web-pivot/src/main/webapp/dhis-web-pivot/app/scripts/plugin.js
@@ -1,38 +1,6 @@
 PT.plugin = {};
 
 Ext.onReady(function() {
-	Ext.Ajax.method = 'GET';
-
-	document.body.oncontextmenu = function() {
-		return false;
-	};
-	
-	// Table css
-	css = 'table.pivot { \n font-family: arial,sans-serif,ubuntu,consolas; \n } \n';
-	css += '.td-nobreak { \n white-space: nowrap; \n } \n';
-	css += '.td-hidden { \n display: none; \n } \n';
-	css += '.td-collapsed { \n display: none; \n } \n';
-	css += 'table.pivot { \n border-collapse: collapse; \n border-spacing: 0px; \n border: 0 none; \n } \n';
-	css += '.pivot td { \n padding: 5px; \n border: \n 1px solid #b2b2b2; \n } \n';
-	css += '.pivot-dim { \n background-color: #dae6f8; \n text-align: center; \n } \n';
-	css += '.pivot-dim.highlighted { \n	background-color: #c5d8f6; \n } \n';
-	css += '.pivot-dim-subtotal { \n background-color: #cad6e8; \n text-align: center; \n } \n';
-	css += '.pivot-dim-total { \n background-color: #bac6d8; \n text-align: center; \n } \n';
-	css += '.pivot-dim-empty { \n background-color: #dae6f8; \n text-align: center; \n } \n';
-	css += '.pivot-value { \n background-color: #fff; \n white-space: nowrap; \n text-align: right; \n } \n';
-	css += '.pivot-value-subtotal { \n background-color: #f4f4f4; \n white-space: nowrap; \n text-align: right; \n } \n';
-	css += '.pivot-value-subtotal-total { \n background-color: #e7e7e7; \n white-space: nowrap; \n text-align: right; \n } \n';
-	css += '.pivot-value-total { \n background-color: #e4e4e4; \n white-space: nowrap; \n text-align: right; \n } \n';
-	css += '.pivot-value-total-subgrandtotal { \n background-color: #d8d8d8; \n white-space: nowrap; \n text-align: right; \n } \n';
-	css += '.pivot-value-grandtotal { \n background-color: #c8c8c8; \n white-space: nowrap; \n text-align: right; \n } \n';
-	
-	// Load mask css
-	
-	css += '.x-mask-msg { \n padding: 0; \n	border: 0 none; \n background-image: none; \n background-color: transparent; \n } \n';
-	css += '.x-mask-msg div { \n background-position: 11px center; \n } \n';
-	css += '.x-mask-msg .x-mask-loading { \n border: 0 none; \n	background-color: #000; \n color: #fff; \n border-radius: 2px; \n padding: 12px 14px 12px 30px; \n opacity: 0.65; \n } \n';	
-	
-	Ext.util.CSS.createStyleSheet(css);
 	
 	PT.plugin.getTable = function(config) {
 		var validateConfig,
@@ -63,66 +31,92 @@ Ext.onReady(function() {
 			
 		afterRender = function() {};
 			
-		createViewport = function() {
-			var el = Ext.get(pt.el),
-				setFavorite,
-				centerRegion;
-				
-			setFavorite = function(layout) {
-				pt.util.pivot.createTable(layout, pt);
-			};
+	};
+	
+	
+	createViewport = function() {
+		var el = Ext.get(pt.el),
+			setFavorite,
+			centerRegion;
 			
-			centerRegion = Ext.create('Ext.panel.Panel', {
-				renderTo: el,
-				bodyStyle: 'border: 0 none',
-				layout: 'fit',
-				listeners: {
-					afterrender: function() {
-						afterRender();
-					}
-				}
-			});
-			
-			return {
-				setFavorite: setFavorite,
-				centerRegion: centerRegion
-			};
+		setFavorite = function(layout) {
+			pt.util.pivot.createTable(layout, pt);
 		};
 		
-		initialize = function() {
-			if (!validateConfig(config)) {
-				return;
-			}
-			
-			pt = PT.core.getInstance({
-				baseUrl: config.url,
-				el: config.el
-			});
-			
-			PT.core.instances.push(pt);
-			
-			pt.viewport = createViewport();
-			pt.isPlugin = true;
-
-			Ext.data.JsonP.request({
-				url: pt.baseUrl + '/dhis-web-pivot/initialize.action',
-				success: function(r) {
-					pt.init = r;
-					
-					if (config.uid) {
-						pt.util.pivot.loadTable(config.uid);
-					}
-					else {
-						layout = pt.api.layout.Layout(config);
-						
-						if (!layout) {
-							return;
-						}
-						
-						pt.util.pivot.createTable(pt, layout);
-					}
+		centerRegion = Ext.create('Ext.panel.Panel', {
+			renderTo: el,
+			bodyStyle: 'border: 0 none',
+			layout: 'fit',
+			listeners: {
+				afterrender: function() {
+					afterRender();
 				}
-			});
-		}();
+			}
+		});
+		
+		return {
+			setFavorite: setFavorite,
+			centerRegion: centerRegion
+		};
 	};
+		
+	initialize = function() {
+		
+		// Ext configuration
+		Ext.Ajax.method = 'GET';
+		
+		// Create CSS
+		css = 'table.pivot { \n font-family: arial,sans-serif,ubuntu,consolas; \n } \n';
+		css += '.td-nobreak { \n white-space: nowrap; \n } \n';
+		css += '.td-hidden { \n display: none; \n } \n';
+		css += '.td-collapsed { \n display: none; \n } \n';
+		css += 'table.pivot { \n border-collapse: collapse; \n border-spacing: 0px; \n border: 0 none; \n } \n';
+		css += '.pivot td { \n padding: 5px; \n border: \n 1px solid #b2b2b2; \n } \n';
+		css += '.pivot-dim { \n background-color: #dae6f8; \n text-align: center; \n } \n';
+		css += '.pivot-dim.highlighted { \n	background-color: #c5d8f6; \n } \n';
+		css += '.pivot-dim-subtotal { \n background-color: #cad6e8; \n text-align: center; \n } \n';
+		css += '.pivot-dim-total { \n background-color: #bac6d8; \n text-align: center; \n } \n';
+		css += '.pivot-dim-empty { \n background-color: #dae6f8; \n text-align: center; \n } \n';
+		css += '.pivot-value { \n background-color: #fff; \n white-space: nowrap; \n text-align: right; \n } \n';
+		css += '.pivot-value-subtotal { \n background-color: #f4f4f4; \n white-space: nowrap; \n text-align: right; \n } \n';
+		css += '.pivot-value-subtotal-total { \n background-color: #e7e7e7; \n white-space: nowrap; \n text-align: right; \n } \n';
+		css += '.pivot-value-total { \n background-color: #e4e4e4; \n white-space: nowrap; \n text-align: right; \n } \n';
+		css += '.pivot-value-total-subgrandtotal { \n background-color: #d8d8d8; \n white-space: nowrap; \n text-align: right; \n } \n';
+		css += '.pivot-value-grandtotal { \n background-color: #c8c8c8; \n white-space: nowrap; \n text-align: right; \n } \n';
+		
+		css += '.x-mask-msg { \n padding: 0; \n	border: 0 none; \n background-image: none; \n background-color: transparent; \n } \n';
+		css += '.x-mask-msg div { \n background-position: 11px center; \n } \n';
+		css += '.x-mask-msg .x-mask-loading { \n border: 0 none; \n	background-color: #000; \n color: #fff; \n border-radius: 2px; \n padding: 12px 14px 12px 30px; \n opacity: 0.65; \n } \n';	
+		
+		Ext.util.CSS.createStyleSheet(css);
+		if (!validateConfig(config)) {
+			return;
+		}
+		
+		// Instance		//alt må inn i gettable
+		pt = PT.core.getInstance(config);
+
+		Ext.data.JsonP.request({
+			url: pt.baseUrl + '/dhis-web-pivot/initialize.action',
+			success: function(r) {
+				pt.init = PT.core.getInits(r);
+				pt.viewport = createViewport();
+				pt.isPlugin = true;
+				
+				if (config.uid) {
+					pt.util.pivot.loadTable(config.uid);
+				}
+				else {
+					layout = pt.api.layout.Layout(config);
+					
+					if (!layout) {
+						return;
+					}
+					
+					pt.util.pivot.createTable(layout, pt);
+				}
+			}
+		});
+	}();
+		
 });
