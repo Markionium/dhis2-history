d6b88ab62fd2a13b687d8d070f6da0e724bd498a
Rosu Ovidiu rosu.ovi@gmail.com <Rosu Ovidiu rosu.ovi@gmail.com>
2013-09-02 21:06:27 +0300

    1. Started working on the Detailed MetaData Import

--- dhis-2/dhis-web/dhis-web-importexport/src/main/webapp/dhis-web-importexport/javascript/detailedMetaDataImport.js
+++ dhis-2/dhis-web/dhis-web-importexport/src/main/webapp/dhis-web-importexport/javascript/detailedMetaDataImport.js
@@ -1,1 +1,37 @@
 //@author Ovidiu Rosu <rosu.ovi@gmail.com>
+
+$( document ).ready( function() {
+    pingNotificationsTimeout();
+} );
+
+var pingTimeout = null;
+
+function importDetailedMetaData()
+{
+    if ( !$( "#upload" ).val() )
+    {
+        setHeaderDelayMessage( "Please select a file to upload" );
+        return false;
+    }
+
+    $( "#notificationTable" ).empty();
+    $( "#importForm" ).submit();
+}
+
+function pingNotificationsTimeout()
+{
+    pingNotifications( 'METADATA_IMPORT', 'notificationTable', displaySummaryLink );
+    pingTimeout = setTimeout( "pingNotificationsTimeout()", 2500 );
+}
+
+function displaySummaryLink()
+{
+    var html = '<tr><td></td><td><a href="javascript:displaySummary()">Display import summary</a></td></tr>';
+    $( '#notificationTable' ).prepend( html );
+}
+
+function displaySummary()
+{
+    $( '#notificationDiv' ).hide();
+    $( '#importSummaryDiv' ).show( 'fast' ).load( 'getMetaDataImportSummary.action' );
+}
