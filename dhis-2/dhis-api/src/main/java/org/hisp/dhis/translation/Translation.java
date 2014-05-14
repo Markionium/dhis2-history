3dbd9b7f1552fd2ff1ab932d622cc3ef0c38dcfb
Kiran Prakash <kprakash@thoughtworks.com>
2014-05-14 16:33:46 +0530

    fixed some serialization issues. added header on serialization

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/translation/Translation.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/translation/Translation.java
@@ -28,22 +28,19 @@ package org.hisp.dhis.translation;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import org.hisp.dhis.common.BaseIdentifiableObject;
 import org.hisp.dhis.common.DxfNamespaces;
-import org.hisp.dhis.common.view.DetailedView;
-import org.hisp.dhis.common.view.ExportView;
 
-import java.io.Serializable;
 import java.util.Date;
 
 /**
  * @author Oyvind Brucker
  */
-@JacksonXmlRootElement( localName = "dataElement", namespace = DxfNamespaces.DXF_2_0 )
+@JacksonXmlRootElement( localName = "translation", namespace = DxfNamespaces.DXF_2_0 )
 public class Translation
     extends BaseIdentifiableObject
 
@@ -85,6 +82,7 @@ public class Translation
     // Logic
     // -------------------------------------------------------------------------
 
+    @JsonIgnore
     public String getClassIdPropKey()
     {
         return className + "-" + uid + "-" + property;
--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/api/controller/TranslationController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/api/controller/TranslationController.java
@@ -1,10 +1,9 @@
 package org.hisp.dhis.api.controller;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import org.hisp.dhis.api.utils.ContextUtils;
 import org.hisp.dhis.dxf2.utils.JacksonUtils;
-import org.hisp.dhis.translation.Translation;
 import org.hisp.dhis.translation.TranslationService;
+import org.hisp.dhis.translation.Translations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collection;
 
 import static org.hisp.dhis.api.utils.ContextUtils.CONTENT_TYPE_JSON;
 
@@ -22,7 +20,7 @@ import static org.hisp.dhis.api.utils.ContextUtils.CONTENT_TYPE_JSON;
  */
 
 @Controller
-@RequestMapping( value = TranslationController.RESOURCE_PATH )
+@RequestMapping(value = TranslationController.RESOURCE_PATH)
 public class TranslationController
 {
     public static final String RESOURCE_PATH = "/translations";
@@ -38,17 +36,15 @@ public class TranslationController
     @RequestMapping( produces = CONTENT_TYPE_JSON, method = RequestMethod.GET )
     public void exportJson( HttpServletResponse response ) throws IOException
     {
-        Collection<Translation> allTranslations = translationService.getAllTranslations();
         contextUtils.configureResponse( response, ContextUtils.CONTENT_TYPE_JSON, ContextUtils.CacheStrategy.NO_CACHE, "translations.json", false );
-        JacksonUtils.toJson( response.getOutputStream(), allTranslations );
+        JacksonUtils.toJson( response.getOutputStream(), new Translations( translationService.getAllTranslations() ) );
     }
 
-    @RequestMapping( method = RequestMethod.POST, consumes = CONTENT_TYPE_JSON )
+    @RequestMapping(method = RequestMethod.POST, consumes = CONTENT_TYPE_JSON)
     public void importJson( HttpServletRequest request ) throws IOException
     {
-        TypeReference<Collection<Translation>> typeReference = new TypeReference<Collection<Translation>>(){};
-        Collection<Translation> translations = JacksonUtils.fromJson( request.getInputStream(), typeReference );
-        translationService.createOrUpdate( translations );
+        Translations translations = JacksonUtils.fromJson( request.getInputStream(), Translations.class );
+        translationService.createOrUpdate( translations.getTranslations() );
     }
 
 }
