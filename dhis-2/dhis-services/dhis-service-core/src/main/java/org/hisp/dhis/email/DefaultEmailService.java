533ee80252138b99556790a346b4e058e0a00fb9
Halvdan Hoem Grelland <halvdanhg@gmail.com>
2014-06-16 16:31:54 +0200

    EmailController and test-email implementation update

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/email/DefaultEmailService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/email/DefaultEmailService.java
@@ -1,8 +1,37 @@
 package org.hisp.dhis.email;
 
+/*
+ * Copyright (c) 2004-2014, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hisp.dhis.message.MessageSender;
+import org.hisp.dhis.setting.SystemSettingManager;
 import org.hisp.dhis.user.CurrentUserService;
 import org.hisp.dhis.user.User;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +40,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * @author Halvdan Hoem Grelland
+ * @author Halvdan Hoem Grelland <halvdanhg@gmail.com>
  */
 @Transactional
 public class DefaultEmailService
@@ -19,8 +48,8 @@ public class DefaultEmailService
 {
     private static final Log log = LogFactory.getLog( DefaultEmailService.class );
 
-    private static final String TEST_EMAIL_SUBJECT = "Test message from DHIS 2";
-    private static final String TEST_EMAIL_TEXT = "This is a test message from DHIS 2";
+    private static final String TEST_EMAIL_SUBJECT = "Test email from DHIS 2";
+    private static final String TEST_EMAIL_TEXT = "This is an automatically generated email from ";
 
     // -------------------------------------------------------------------------
     // Dependencies
@@ -35,9 +64,16 @@ public class DefaultEmailService
 
     private CurrentUserService currentUserService;
 
-    public void setCurrentUserService(CurrentUserService currentUserService)
+    public void setCurrentUserService( CurrentUserService currentUserService )
     {
         this.currentUserService = currentUserService;
+    }
+
+    private SystemSettingManager systemSettingManager;
+
+    public void setSystemSettingManager( SystemSettingManager systemSettingManager )
+    {
+        this.systemSettingManager = systemSettingManager;
     }
 
     // -------------------------------------------------------------------------
@@ -45,11 +81,18 @@ public class DefaultEmailService
     // -------------------------------------------------------------------------
 
     @Override
+    public boolean emailEnabled()
+    {
+        return systemSettingManager.emailEnabled();
+    }
+
+    @Override
     public void sendEmail( String subject, String text, User sender, User recipient, boolean forceSend )
     {
         Set<User> recipients = new HashSet<User>( );
         recipients.add( recipient );
 
+        /* Method is called asynchronously, must therefore re-instantiate HashSet in method call */
         emailMessageSender.sendMessage( subject, text, sender, new HashSet<User>( recipients ), forceSend );
     }
 
@@ -60,8 +103,9 @@ public class DefaultEmailService
     }
 
     @Override
-    public void sendTestEmail( User recipient )
+    public void sendTestEmail( )
     {
-        sendEmail(TEST_EMAIL_SUBJECT, TEST_EMAIL_TEXT, null, recipient, true);
+        String instanceName = systemSettingManager.getSystemSetting( systemSettingManager.KEY_APPLICATION_TITLE ).toString();
+        sendEmail( TEST_EMAIL_SUBJECT, TEST_EMAIL_TEXT + instanceName, null, currentUserService.getCurrentUser(), true );
     }
 }
--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/EmailController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/EmailController.java
@@ -1,5 +1,33 @@
 package org.hisp.dhis.webapi.controller;
 
+/*
+ * Copyright (c) 2004-2014, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hisp.dhis.email.EmailService;
@@ -38,9 +66,29 @@ public class EmailController
     @RequestMapping( value = "/sendTestEmail" , method = RequestMethod.POST, produces = ContextUtils.CONTENT_TYPE_TEXT )
     public @ResponseBody String sendTestEmail( HttpServletRequest request, HttpServletResponse response )
     {
-        emailService.sendTestEmail( currentUserService.getCurrentUser( ) );
+        String responseMessage;
+        String userEmail = currentUserService.getCurrentUser().getEmail();
+        boolean smtpConfigured = emailService.emailEnabled();
+        boolean userEmailConfigured = userEmail != null && !userEmail.isEmpty();
 
-        response.setStatus( HttpServletResponse.SC_OK );
-        return "A test email has been sent to you";
+        if( smtpConfigured && userEmailConfigured )
+        {
+            response.setStatus( HttpServletResponse.SC_OK );
+            emailService.sendTestEmail( );
+
+            responseMessage =  "A test email was sent to " + userEmail;
+        }
+        else if( userEmailConfigured )
+        {
+            response.setStatus( HttpServletResponse.SC_ACCEPTED );
+            responseMessage = "Could not send test email to " + userEmail + ": SMTP is not configured";
+        }
+        else /* smtpConfigured */
+        {
+            response.setStatus( HttpServletResponse.SC_ACCEPTED );
+            responseMessage = "Could not send test email: no user email address configured";
+        }
+
+        return responseMessage;
     }
 }
