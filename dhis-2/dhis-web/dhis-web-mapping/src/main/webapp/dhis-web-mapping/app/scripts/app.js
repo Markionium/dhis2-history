cf8a5f73e85b091e955fb12fabbabc43e7c26a9b
Jan Henrik Overland <janhenrik.overland@gmail.com>
2014-07-03 14:41:11 +0200

    gis search win

--- dhis-2/dhis-web/dhis-web-mapping/src/main/webapp/dhis-web-mapping/app/scripts/app.js
+++ dhis-2/dhis-web/dhis-web-mapping/src/main/webapp/dhis-web-mapping/app/scripts/app.js
@@ -1894,105 +1894,101 @@ Ext.onReady( function() {
 
 		button = Ext.create('Ext.ux.button.ColorButton', {
 			width: gis.conf.layout.tool.item_width - gis.conf.layout.tool.itemlabel_width,
+            height: 24,
 			value: '0000ff'
 		});
 
 		window = Ext.create('Ext.window.Window', {
 			title: GIS.i18n.organisationunit_search,
-			layout: 'fit',
 			iconCls: 'gis-window-title-icon-search',
-			cls: 'gis-container-default',
-			width: gis.conf.layout.tool.window_width,
+            bodyStyle: 'background-color: #fff; padding: 1px',
 			resizable: false,
-			height: 400,
+			height: 380,
 			items: [
-				{
-					cls: 'gis-container-inner',
-					items: [
-						{
-							layout: 'column',
-							cls: 'gis-container-inner',
-							items: [
-								{
-									cls: 'gis-panel-html-label',
-									html: GIS.i18n.highlight_color + ':',
-									width: gis.conf.layout.tool.itemlabel_width
-								},
-								button
-							]
-						},
-						{
-							cls: 'gis-panel-html-separator'
-						},
-						{
-							layout: 'column',
-							cls: 'gis-container-inner',
-							items: [
-								{
-									cls: 'gis-panel-html-label',
-									html: GIS.i18n.text_filter + ':',
-									width: gis.conf.layout.tool.itemlabel_width
-								},
-								{
-									xtype: 'textfield',
-									cls: 'gis-textfield',
-									width: gis.conf.layout.tool.item_width - gis.conf.layout.tool.itemlabel_width,
-									enableKeyEvents: true,
-									listeners: {
-										keyup: function() {
-											store.clearFilter();
-											if (this.getValue()) {
-												store.filter('name', this.getValue());
-											}
-											store.sortStore();
-										}
-									}
-								}
-							]
-						},
-						{
-							xtype: 'grid',
-							cls: 'gis-grid',
-							height: 290,
-							width: gis.conf.layout.tool.item_width,
-							scroll: 'vertical',
-							hideHeaders: true,
-							columns: [{
-								id: 'name',
-								text: 'Organisation units',
-								dataIndex: 'name',
-								sortable: false,
-								width: gis.conf.layout.tool.item_width
-							}],
-							store: layer.core.featureStore,
-							listeners: {
-								select: function(grid, record) {
-									var feature = layer.getFeaturesByAttribute('id', record.data.id)[0],
-										color = button.getValue(),
-										symbolizer;
+                {
+                    layout: 'column',
+                    cls: 'gis-container-inner',
+                    items: [
+                        {
+                            cls: 'gis-panel-html-label',
+                            html: GIS.i18n.highlight_color + ':',
+                            width: gis.conf.layout.tool.itemlabel_width
+                        },
+                        button
+                    ]
+                },
+                {
+                    xtype: 'container',
+                    height: 1
+                },
+                {
+                    layout: 'column',
+                    cls: 'gis-container-inner',
+                    items: [
+                        {
+                            cls: 'gis-panel-html-label',
+                            html: GIS.i18n.text_filter + ':',
+                            width: gis.conf.layout.tool.itemlabel_width
+                        },
+                        {
+                            xtype: 'textfield',
+                            cls: 'gis-textfield',
+                            width: gis.conf.layout.tool.item_width - gis.conf.layout.tool.itemlabel_width,
+                            enableKeyEvents: true,
+                            listeners: {
+                                keyup: function() {
+                                    store.clearFilter();
+                                    if (this.getValue()) {
+                                        store.filter('name', this.getValue());
+                                    }
+                                    store.sortStore();
+                                }
+                            }
+                        }
+                    ]
+                },
+                {
+                    xtype: 'grid',
+                    cls: 'gis-grid',
+                    bodyStyle: 'border: 0 none',
+                    height: 290,
+                    width: gis.conf.layout.tool.item_width,
+                    scroll: 'vertical',
+                    hideHeaders: true,
+                    columns: [{
+                        id: 'name',
+                        text: 'Organisation units',
+                        dataIndex: 'name',
+                        sortable: false,
+                        width: gis.conf.layout.tool.item_width
+                    }],
+                    store: layer.core.featureStore,
+                    listeners: {
+                        select: function(grid, record) {
+                            var feature = layer.getFeaturesByAttribute('id', record.data.id)[0],
+                                color = button.getValue(),
+                                symbolizer;
 
-									layer.redraw();
+                            layer.redraw();
 
-									if (feature.geometry.CLASS_NAME === gis.conf.finals.openLayers.point_classname) {
-										symbolizer = new OpenLayers.Symbolizer.Point({
-											pointRadius: 6,
-											fillColor: '#' + color,
-											strokeWidth: 1
-										});
-									}
-									else {
-										symbolizer = new OpenLayers.Symbolizer.Polygon({
-											strokeColor: '#' + color,
-											fillColor: '#' + color
-										});
-									}
+                            if (feature.geometry.CLASS_NAME === gis.conf.finals.openLayers.point_classname) {
+                                symbolizer = new OpenLayers.Symbolizer.Point({
+                                    pointRadius: 6,
+                                    fillColor: '#' + color,
+                                    strokeWidth: 1
+                                });
+                            }
+                            else {
+                                symbolizer = new OpenLayers.Symbolizer.Polygon({
+                                    strokeColor: '#' + color,
+                                    fillColor: '#' + color
+                                });
+                            }
 
-									layer.drawFeature(feature, symbolizer);
-								}
-							}
-						}
-					]
-				}
+                            layer.drawFeature(feature, symbolizer);
+                        }
+                    }
+                }
 			],
 			listeners: {
 				render: function() {
