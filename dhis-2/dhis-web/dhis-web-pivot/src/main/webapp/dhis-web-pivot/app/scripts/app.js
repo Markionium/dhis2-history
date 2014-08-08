ac174f3a7202b77416dc1f233a72aff36c96f94d
Jan Henrik Overland <janhenrik.overland@gmail.com>
2014-08-08 13:08:16 +0200

    pt cal tmp

--- dhis-2/dhis-web/dhis-web-pivot/src/main/webapp/dhis-web-pivot/app/scripts/app.js
+++ dhis-2/dhis-web/dhis-web-pivot/src/main/webapp/dhis-web-pivot/app/scripts/app.js
@@ -6057,21 +6057,28 @@ Ext.onReady( function() {
 							success: function(r) {
                                 var info = Ext.decode(r.responseText),
                                     dhis2PeriodUrl = '../../dhis-web-commons/javascripts/dhis2/dhis2.period.js',
+                                    calendarMap = {'iso8601': 'gregorian'},
                                     calendarUrl;
 
                                 // calendar
-                                init.calendar = info.calendar || 'iso8601';
+                                init.calendar = calendarMap[info.calendar] || info.calendar || 'gregorian';
+                                init.dateFormat = info.dateFormat || 'yyyy-mm-dd';
 
-                                if (Ext.Array.contains(['coptic', 'ethiopian', 'islamic', 'julian', 'nepali', 'thai'], init.calendar)) {
-                                    calendarUrl = '../../dhis-web-commons/javascripts/jQuery/calendars/jquery.calendars.' + init.calendar + '.min.js';
+                                //if (Ext.Array.contains(['coptic', 'ethiopian', 'islamic', 'julian', 'nepali', 'thai'], init.calendar)) {
+                                    //calendarUrl = '../../dhis-web-commons/javascripts/jQuery/calendars/jquery.calendars.' + init.calendar + '.min.js';
 
-                                    Ext.Loader.injectScriptElement(calendarUrl, function() {
-                                        Ext.Loader.injectScriptElement(dhis2PeriodUrl, function() {});
-                                    });
-                                }
-                                else {
-                                    Ext.Loader.injectScriptElement(dhis2PeriodUrl, function() {});
-                                }
+                                    //Ext.Loader.injectScriptElement(calendarUrl, function() {
+                                        //Ext.Loader.injectScriptElement(dhis2PeriodUrl, function() {});
+                                    //});
+                                //}
+                                //else {
+                                    //Ext.Loader.injectScriptElement(dhis2PeriodUrl, function() {
+                                        //var cal = $.calendars.instance('gregorian'),
+                                            //gen = new dhis2.period.PeriodGenerator(cal, init.dateFormat);
+
+                                        //console.log(gen.generateReversedPeriods('Yearly', 0));
+                                    //});
+                                //}
 
                                 // context path
 								init.contextPath = info.contextPath || init.contextPath;
