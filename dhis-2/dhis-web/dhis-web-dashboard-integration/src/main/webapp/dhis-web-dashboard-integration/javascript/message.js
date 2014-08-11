337d47b43e5346331b0a526e8af232e6bfff0c8d
Halvdan Hoem Grelland <halvdanhg@gmail.com>
2014-08-11 13:51:35 +0200

    refactor

--- dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/message.js
+++ dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/message.js
@@ -30,6 +30,7 @@ function removeMessages( messages )
                     $( "#messages" ).find( "[name='" + response.removed[i] + "']" ).remove();
                 }
                 setHeaderDelayMessage( response );
+                $( "#multiSelectCheckbox" ).removeAttr( "checked" );
             },
             error: function( response )
             {
@@ -41,22 +42,23 @@ function removeMessages( messages )
 
 function markMessagesRead( messages )
 {
-  $.ajax(
-  {
-      url: "../../api/messageConversations/read",
-      type: "PUT",
-      data: JSON.stringify( messages.uid ),
-      contentType: "application/json",
-      dataType: "json",
-      success: function( response )
-      {
-          toggleMessagesRead( messages.markedRead );
-      },
-      error: function( response )
-      {
-          showErrorMessage( response.message );
-      }
-  });
+    $.ajax(
+    {
+        url: "../../api/messageConversations/read",
+        type: "PUT",
+        data: JSON.stringify( messages.uid ),
+        contentType: "application/json",
+        dataType: "json",
+        success: function( response )
+        {
+            toggleMessagesRead( response.markedRead );
+            $( "#multiSelectCheckbox" ).removeAttr( "checked" );
+        },
+        error: function( response )
+        {
+            showErrorMessage( response.message );
+        }
+    });
 }
 
 function markMessagesUnread( messages )
@@ -71,6 +73,7 @@ function markMessagesUnread( messages )
         success: function( response )
         {
             toggleMessagesRead( response.markedUnread );
+            $( "#multiSelectCheckbox" ).removeAttr( "checked" );
         },
         error: function( response )
         {
@@ -87,7 +90,6 @@ function toggleMessagesRead( messageUids )
     {
         messages.find( "[name='" + messageUids[i] + "']" ).toggleClass( "unread bold" );
         messages.find( "input:checkbox" ).removeAttr( "checked" );
-        $( "#toggleAllCheckbox" ).removeAttr( "checked" );
     }
 }
 
@@ -179,4 +181,90 @@ function formatItem( item )
     {
         return item.text;
     }
+}
+
+/* Multi select checkbox button */
+function multiSelect()
+{
+    $( "#multiSelectButton" ).click(function( event )
+    {
+        $( document ).one( "click", function()
+        {
+            $( "#multiSelectMenu" ).css( "visibility", "hidden" );
+        });
+
+        var $multiMenu = $( "#multiSelectMenu" );
+
+        if( $multiMenu.css( "visibility" ) !== "visible" )
+        {
+            $multiMenu.css( "visibility", "visible" );
+        }
+        else
+        {
+            $multiMenu.css( "visibility", "hidden" );
+        }
+        event.stopPropagation();
+    });
+
+    $( "#multiSelectMenu" )
+        .css( "visibility", "hidden" )
+        .position(
+        {
+            my: "left top",
+            at: "left bottom",
+            of: "#multiSelectButton"
+        });
+
+    $( "#multiSelectCheckbox" ).click( function( event )
+    {
+        if( typeof $( this ).attr( "checked" ) !== "undefined" ) {
+            $( "#messages input:checkbox" ).attr( "checked" , "checked" );
+        }
+        else {
+            $( "#messages input:checkbox:checked" ).removeAttr( "checked" );
+        }
+        event.stopPropagation();
+    });
+
+    $( "#multiSelectMenu li, #multiSelectMenu li a" ).click(function()
+    {
+        var messages = {};
+        messages.uid = [];
+        $( "#messages input:checkbox:checked" ).each(function() {
+            messages.uid.push(this.value);
+        });
+
+        if( messages.uid.length < 1 )
+        {
+            showErrorMessage(i18n_no_messages_selected);
+            return;
+        }
+
+        var action = $(this).attr( "data-action" );
+
+        if( action === "delete" ) {
+            removeMessages(messages);
+        }
+        else if( action === "markRead" ) {
+            markMessagesRead(messages);
+        }
+        else if( action === "markUnread" ) {
+            markMessagesUnread(messages);
+        }
+    });
+
+    /* Checkboxes all selected/deselected trigger select/deselect of multi select checkbox */
+
+    var numCheckboxes = $( "#messages input:checkbox" ).length;
+
+    $( "#messages input:checkbox" ).click( function()
+    {
+        var checked = $( "#messages input:checkbox:checked" );
+        if( checked.length < 1 ) {
+        $( "#multiSelectCheckbox" ).removeAttr( "checked" );
+        }
+        else if( checked.length === numCheckboxes ) {
+        $( "#multiSelectCheckbox" ).attr( "checked", "checked" );
+        }
+    });
 }
