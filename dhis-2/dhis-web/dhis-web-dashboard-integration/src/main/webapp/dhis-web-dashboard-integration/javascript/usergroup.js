6ac4806ae2ac8fbf95641a66dcfdf3a4291f5ee4
Halvdan Hoem Grelland <halvdanhg@gmail.com>
2014-10-17 16:05:41 +0200

    wip

--- dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/usergroup.js
+++ dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/usergroup.js
@@ -31,24 +31,48 @@ function removeUserGroup( context ) {
 function joinUserGroup( context ) {
     // TODO pre-checks
 
-    jQuery.post( 'joinUserGroup.action', { userGroupUid: context.uid },
-      function( json ) {
-          if( json.response === "success" )
-          {
-            // TODO
-          }
-          else // Error
-          {
+    $.ajax( {
+        type: "POST",
+        dataType: "json",
+        url: "joinUserGroup.action",
+        data: { userGroupId: context.id },
+        accepts: "application/json",
+        success: function( json ) {
+            if( $.parseJSON( json ).response == "success" )
+            {
+                console.log( "joined group " + context.id );
+                $( "#tr" + context.id ).children().find( ".fa-user" ).show();
+            }
+        },
+        error: function( json ) {
 
-          }
-      }
-    );
+        }
+    } );
+//    jQuery.postJSON( 'joinUserGroup.action', { userGroupUid: context.uid },
+//      function( json ) {
+//          if( json.response === "success" )
+//          {
+//              $( "#tr" + context.id ).children().find( ".fa-user" ).show();
+//          }
+//          else // Error
+//          {
+//
+//          }
+//      }
+//    );
 }
 
 function leaveUserGroup( context ) {
-    jQuery.post( 'leaveUserGroup.action', { userGroupUid: context.uid },
+    jQuery.postJSON( 'leaveUserGroup.action', { userGroupUid: context.uid },
       function( json ) {
-        console.log( json )
+        if( json.response === "success" )
+        {
+            $( "#tr" + context.id ).children().find( ".fa-user" ).hide();
+        }
+        else // Error
+        {
+
+        }
       }
     );
 }
