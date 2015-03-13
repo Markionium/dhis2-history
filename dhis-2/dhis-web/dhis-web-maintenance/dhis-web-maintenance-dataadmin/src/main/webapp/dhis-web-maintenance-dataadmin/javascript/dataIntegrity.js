107f5b5696270265a796a00a66e897389fab8c52
Halvdan Hoem Grelland <halvdanhg@gmail.com>
2015-03-13 16:25:04 +0100

    wip

--- dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-dataadmin/src/main/webapp/dhis-web-maintenance-dataadmin/javascript/dataIntegrity.js
+++ dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-dataadmin/src/main/webapp/dhis-web-maintenance-dataadmin/javascript/dataIntegrity.js
@@ -4,7 +4,8 @@ $( document ).ready( function() {
     $.ajax({
         url: '../api/dataIntegrity',
         method: 'POST',
-        success: pollDataIntegrityCheckFinished,
+        //success: pollDataIntegrityCheckFinished,
+        success: pollDataIntegrityCheckFinished2,
         error: function( xhr, txtStatus, err ) {
             showErrorMessage( "Data integrity checks cannot be run. Request failed.", 3 );
             throw Error( xhr.responseText );
@@ -25,6 +26,96 @@ function pollDataIntegrityCheckFinished() {
         } );
     } );
     checkFinishedTimeout = setTimeout( "pollDataIntegrityCheckFinished()", 1500 );
+}
+
+function pollDataIntegrityCheckFinished2() {
+    pingNotifications( 'DATAINTEGRITY', 'notificationsTable', function() {
+        $.getJSON( "../../api/system/taskSummaries/dataintegrity", {}, function( json ) {
+            hideLoader();
+            $( "#di-title" ).hide();
+            $( "#di-completed" ).show();
+            populateIntegrityItems2( json );
+            clearTimeout( checkFinishedTimeout );
+        } );
+    } );
+    checkFinishedTimeout = setTimeout( "pollDataIntegrityCheckFinished2()", 1500 );
+}
+
+function populateIntegrityItems2( json ) {
+    console.log( json );
+
+    displayViolationList2( json.dataElementsWithoutDataSet, "dataElementsWithoutDataSet", false );
+    displayViolationList2( json.dataElementsWithoutGroups, "dataElementsWithoutGroups", false );
+    displayViolationList2( json.dataElementsViolatingExclusiveGroupSets, "dataElementsViolatingExclusiveGroupSets", true );
+    displayViolationList2( json.dataElementsInDataSetNotInForm, "dataElementsInDataSetNotInForm", true );
+    displayViolationList2( json.dataElementsAssignedToDataSetsWithDifferentPeriodTypes, "dataElementsAssignedToDataSetsWithDifferentPeriodTypes", true );
+    displayViolationList2( json.categoryOptionCombosNotInDataElementCategoryCombo, "categoryOptionCombosNotInDataElementCategoryCombo", true );
+    displayViolationList2( json.dataSetsNotAssignedToOrganisationUnits, "dataSetsNotAssignedToOrganisationUnits", false );
+    displayViolationList2( json.sectionsWithInvalidCategoryCombinations, "sectionsWithInvalidCategoryCombinations", false );
+    displayViolationList2( json.indicatorsWithIdenticalFormulas, "indicatorsWithIdenticalFormulas", false );
+    displayViolationList2( json.indicatorsWithoutGroups, "indicatorsWithoutGroups", false );
+    displayViolationList2( json.invalidIndicatorNumerators, "invalidIndicatorNumerators", true );
+    displayViolationList2( json.invalidIndicatorDenominators, "invalidIndicatorDenominators", true );
+    displayViolationList2( json.indicatorsViolatingExclusiveGroupSets, "indicatorsViolatingExclusiveGroupSets", true );
+    displayViolationList2( json.organisationUnitsWithCyclicReferences, "organisationUnitsWithCyclicReferences", false );
+    displayViolationList2( json.orphanedOrganisationUnits, "orphanedOrganisationUnits", false );
+    displayViolationList2( json.organisationUnitsWithoutGroups, "organisationUnitsWithoutGroups", false );
+    displayViolationList2( json.organisationUnitsViolatingExclusiveGroupSets, "organisationUnitsViolatingExclusiveGroupSets", true );
+    displayViolationList2( json.organisationUnitGroupsWithoutGroupSets, "organisationUnitGroupsWithoutGroupSets", false );
+    displayViolationList2( json.duplicatePeriods, "duplicatePeriods", false );
+    displayViolationList2( json.validationRulesWithoutGroups, "validationRulesWithoutGroups", false );
+    displayViolationList2( json.invalidValidationRuleLeftSideExpressions, "invalidValidationRuleLeftSideExpressions", true );
+    displayViolationList2( json.invalidValidationRuleRightSideExpressions, "invalidValidationRuleRightSideExpressions", true );
+
+}
+
+function displayViolationList2( list, id, lineBreak ) {
+    var renderWithViolations = function( jqButton, jqContainer ) {
+        jqButton
+            .attr( { src: "../images/down.png", title: "View violations" } )
+            .css( { cursor: "pointer" } )
+            .click( function() { jqContainer.slideToggle( "fast" ); } );
+    };
+
+    var renderNoViolations = function( jqButton ) {
+        jqButton.attr({ src: "../images/check.png", title: "No violations" });
+    };
+
+    var $button = $( "#" + id + "Button" );
+    var $container = $( "#" + id + "Div" );
+
+    if ( typeof list !== "undefined" ) {
+        renderWithViolations( $button, $container );
+
+        var violations = "";
+
+        if( Array.isArray( list ) && list.length > 0 ) { // TODO Check length elsewhere
+            list.forEach( function( violation ) {
+                violations += violation + "<br>"
+                violations += !!lineBreak ? "<br>" : "";
+            } );
+        } else if ( list !== null && typeof list === "object" ) {
+            console.log( "id: " + id );
+        }
+
+        $container.html( violations );
+    } else {
+        renderNoViolations( $button );
+    }
+
+    $container.hide();
+}
+
+function displayViolationMap2() {
+
+}
+
+function displayViolationCollection2() {
+
+}
+
+function displayViolationWithMapList2() {
+
 }
 
 function populateIntegrityItems( json ) {
