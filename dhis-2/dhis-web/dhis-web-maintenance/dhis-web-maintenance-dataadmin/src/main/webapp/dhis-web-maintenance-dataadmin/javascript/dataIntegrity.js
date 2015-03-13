c70a871efcc3903f3dea3d5ac5a2a95fbfc10496
Halvdan Hoem Grelland <halvdanhg@gmail.com>
2015-03-13 19:26:27 +0100

    Refactor

--- dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-dataadmin/src/main/webapp/dhis-web-maintenance-dataadmin/javascript/dataIntegrity.js
+++ dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-dataadmin/src/main/webapp/dhis-web-maintenance-dataadmin/javascript/dataIntegrity.js
@@ -27,213 +27,105 @@ function pollDataIntegrityCheckFinished() {
     checkFinishedTimeout = setTimeout( "pollDataIntegrityCheckFinished()", 1500 );
 }
 
-var ViolationDisplayTypes = { "list" : 0, "mapList" : 1, "listList" : 2, "map" : 3 };
-
 function populateIntegrityItems( json ) {
-    function displayViolationMap( obj, id, lineBreak ) {
-        var violationsText = "";
-        var isViolations = true;
 
-        if ( typeof obj !== "undefined" ) {
-            for ( var o in obj ) {
-                if ( obj.hasOwnProperty( o ) ) {
-                    violationsText += o + ": " + obj[o];
-                    violationsText += "<br>" + ( !!lineBreak ? "<br>" : "" );
-                }
+    // Render functions
+    var asMap = function( obj, lineBreak ) {
+        var violationsText = "";
+
+        for ( var o in obj ) {
+            if ( obj.hasOwnProperty( o ) ) {
+                violationsText += o + ": " + obj[o];
+                violationsText += "<br>" + ( !!lineBreak ? "<br>" : "" );
             }
-        } else {
-            isViolations = false;
         }
 
-        displayViolation( id, isViolations, violationsText );
-    }
+        return violationsText;
+    };
 
-    function displayViolationCollection( obj, id, lineBreak ) {
+    var asListList = function( obj, lineBreak ) {
         var violationsText = "";
-        var isViolations = true;
 
-        if ( typeof obj !== "undefined" ) {
-            obj.forEach( function( o ) {
-                o.forEach( function( s ) {
+        obj.forEach( function( o ) {
+            o.forEach( function( s ) {
+                violationsText += s + ", ";
+            } );
+            violationsText += "<br>" + ( !!lineBreak ? "<br>" : "" );
+        } );
+
+        return violationsText;
+    };
+
+    var asMapList = function( obj, lineBreak ) {
+        var violationsText = "";
+
+        for ( var o in obj ) {
+            if( obj.hasOwnProperty( o ) ) {
+                violationsText += o + ": ";
+                obj[o].forEach( function( s ) {
                     violationsText += s + ", ";
                 } );
                 violationsText += "<br>" + ( !!lineBreak ? "<br>" : "" );
-            } );
-        } else {
-            isViolations = false;
-        }
-
-        displayViolation( id, isViolations, violationsText );
-    }
-
-    function displayViolationMapList( obj, id, lineBreak ) {
-        var violationsText = "";
-        var isViolations = true;
-
-        if ( typeof obj !== "undefined" ) {
-            for ( var o in obj ) {
-                if( obj.hasOwnProperty( o ) ) {
-                    violationsText += o + ": ";
-                    obj[o].forEach( function( s ) {
-                        violationsText += s + ", ";
-                    } );
-                    violationsText += "<br>" + ( !!lineBreak ? "<br>" : "" );
-                }
             }
-        } else {
-            isViolations = false;
         }
 
-        displayViolation( id, isViolations, violationsText );
-    }
+        return violationsText;
+    };
 
-    function displayViolationList( list, id, lineBreak ) {
+    var asList = function( list, lineBreak ) {
         var violationsText = "";
-        var isViolations = true;
 
-        if ( typeof list !== "undefined" ) {
-            list.forEach( function( violation ) {
-                violationsText += violation + "<br>" + ( !!lineBreak ? "<br>" : "" );
-            } );
-        } else {
-            isViolations = false;
-        }
+        list.forEach( function( violation ) {
+            violationsText += violation + "<br>" + ( !!lineBreak ? "<br>" : "" );
+        } );
 
-        displayViolation( id, isViolations, violationsText );
-    }
+        return violationsText;
+    };
 
+    // Displays an item using the selected renderer function
 
-    function displayViolation( id, isViolation, violationsText ) {
+    var displayViolation = function( obj, id, lineBreak, renderFunc ) {
 
         var $button = $( "#" + id + "Button" );
         var $container = $( "#" + id + "Div" );
 
-        if ( isViolation ) {
+        if ( typeof obj !== "undefined" ) {
             $button
                 .attr( { src: "../images/down.png", title: "View violations" } )
                 .css( { cursor: "pointer" } )
                 .click( function() { $container.slideToggle( "fast" ); } );
 
-            $container.html( violationsText );
+            $container.html( renderFunc( obj, lineBreak ) );
+
         } else {
             $button.attr({ src: "../images/check.png", title: "No violations" } );
         }
 
         $container.hide();
-    }
+    };
 
-    displayViolationList( json.dataElementsWithoutDataSet, "dataElementsWithoutDataSet", false, ViolationDisplayTypes.list );
-    displayViolationList( json.dataElementsWithoutGroups, "dataElementsWithoutGroups", false, "list" );
-    displayViolationMapList( json.dataElementsViolatingExclusiveGroupSets, "dataElementsViolatingExclusiveGroupSets", true, "mapList" );
-    displayViolationMapList( json.dataElementsInDataSetNotInForm, "dataElementsInDataSetNotInForm", true, "mapList" );
-    displayViolationMapList( json.dataElementsAssignedToDataSetsWithDifferentPeriodTypes, "dataElementsAssignedToDataSetsWithDifferentPeriodTypes", true, "mapList" );
-    displayViolationMapList( json.categoryOptionCombosNotInDataElementCategoryCombo, "categoryOptionCombosNotInDataElementCategoryCombo", true, "mapList" );
-    displayViolationList( json.dataSetsNotAssignedToOrganisationUnits, "dataSetsNotAssignedToOrganisationUnits", false, "list" );
-    displayViolationList( json.sectionsWithInvalidCategoryCombinations, "sectionsWithInvalidCategoryCombinations", false, "list" );
-    displayViolationCollection( json.indicatorsWithIdenticalFormulas, "indicatorsWithIdenticalFormulas", false, "listList" );
-    displayViolationList( json.indicatorsWithoutGroups, "indicatorsWithoutGroups", false, "list" );
-    displayViolationMap( json.invalidIndicatorNumerators, "invalidIndicatorNumerators", true, "map" );
-    displayViolationMap( json.invalidIndicatorDenominators, "invalidIndicatorDenominators", true, "map" );
-    displayViolationMapList( json.indicatorsViolatingExclusiveGroupSets, "indicatorsViolatingExclusiveGroupSets", true, "mapList" );
-    displayViolationList( json.duplicatePeriods, "duplicatePeriods", false, "list" );
-    displayViolationList( json.organisationUnitsWithCyclicReferences, "organisationUnitsWithCyclicReferences", false, "list" );
-    displayViolationList( json.orphanedOrganisationUnits, "orphanedOrganisationUnits", false, "list" );
-    displayViolationList( json.organisationUnitsWithoutGroups, "organisationUnitsWithoutGroups", false, "list" );
-    displayViolationMapList( json.organisationUnitsViolatingExclusiveGroupSets, "organisationUnitsViolatingExclusiveGroupSets", true, "mapList" );
-    displayViolationList( json.organisationUnitGroupsWithoutGroupSets, "organisationUnitGroupsWithoutGroupSets", false, "list" );
-    displayViolationList( json.validationRulesWithoutGroups, "validationRulesWithoutGroups", false, "list" );
-    displayViolationMapList( json.invalidValidationRuleLeftSideExpressions, "invalidValidationRuleLeftSideExpressions", true, "mapList" );
-    displayViolationMapList( json.invalidValidationRuleRightSideExpressions, "invalidValidationRuleRightSideExpressions", true, "mapList" );
+    // Display each reported item
+
+    displayViolation( json.dataElementsWithoutDataSet, "dataElementsWithoutDataSet", false, asList );
+    displayViolation( json.dataElementsWithoutGroups, "dataElementsWithoutGroups", false, asList );
+    displayViolation( json.dataElementsViolatingExclusiveGroupSets, "dataElementsViolatingExclusiveGroupSets", true, asMapList );
+    displayViolation( json.dataElementsInDataSetNotInForm, "dataElementsInDataSetNotInForm", true, asMapList );
+    displayViolation( json.dataElementsAssignedToDataSetsWithDifferentPeriodTypes, "dataElementsAssignedToDataSetsWithDifferentPeriodTypes", true, asMapList );
+    displayViolation( json.categoryOptionCombosNotInDataElementCategoryCombo, "categoryOptionCombosNotInDataElementCategoryCombo", true, asMapList );
+    displayViolation( json.dataSetsNotAssignedToOrganisationUnits, "dataSetsNotAssignedToOrganisationUnits", false, asList );
+    displayViolation( json.sectionsWithInvalidCategoryCombinations, "sectionsWithInvalidCategoryCombinations", false, asList );
+    displayViolation( json.indicatorsWithIdenticalFormulas, "indicatorsWithIdenticalFormulas", false, asListList );
+    displayViolation( json.indicatorsWithoutGroups, "indicatorsWithoutGroups", false, asList );
+    displayViolation( json.invalidIndicatorNumerators, "invalidIndicatorNumerators", true, asMap );
+    displayViolation( json.invalidIndicatorDenominators, "invalidIndicatorDenominators", true, asMap );
+    displayViolation( json.indicatorsViolatingExclusiveGroupSets, "indicatorsViolatingExclusiveGroupSets", true, asMapList );
+    displayViolation( json.duplicatePeriods, "duplicatePeriods", false, asList );
+    displayViolation( json.organisationUnitsWithCyclicReferences, "organisationUnitsWithCyclicReferences", false, asList );
+    displayViolation( json.orphanedOrganisationUnits, "orphanedOrganisationUnits", false, asList );
+    displayViolation( json.organisationUnitsWithoutGroups, "organisationUnitsWithoutGroups", false, asList );
+    displayViolation( json.organisationUnitsViolatingExclusiveGroupSets, "organisationUnitsViolatingExclusiveGroupSets", true, asMapList );
+    displayViolation( json.organisationUnitGroupsWithoutGroupSets, "organisationUnitGroupsWithoutGroupSets", false, asList );
+    displayViolation( json.validationRulesWithoutGroups, "validationRulesWithoutGroups", false, asList );
+    displayViolation( json.invalidValidationRuleLeftSideExpressions, "invalidValidationRuleLeftSideExpressions", true, asMapList );
+    displayViolation( json.invalidValidationRuleRightSideExpressions, "invalidValidationRuleRightSideExpressions", true, asMapList );
 }
-
-//function displayViolationMap( obj, id, lineBreak ) {
-//    var violationsText = "";
-//    var isViolations = true;
-//
-//    if ( typeof obj !== "undefined" ) {
-//        for ( var o in obj ) {
-//            if ( obj.hasOwnProperty( o ) ) {
-//                violationsText += o + ": " + obj[o];
-//                violationsText += "<br>" + ( !!lineBreak ? "<br>" : "" );
-//            }
-//        }
-//    } else {
-//        isViolations = false;
-//    }
-//
-//    displayViolation( id, isViolations, violationsText );
-//}
-//
-//function displayViolationCollection( obj, id, lineBreak ) {
-//    var violationsText = "";
-//    var isViolations = true;
-//
-//    if ( typeof obj !== "undefined" ) {
-//        obj.forEach( function( o ) {
-//            o.forEach( function( s ) {
-//                violationsText += s + ", ";
-//            } );
-//            violationsText += "<br>" + ( !!lineBreak ? "<br>" : "" );
-//        } );
-//    } else {
-//        isViolations = false;
-//    }
-//
-//    displayViolation( id, isViolations, violationsText );
-//}
-//
-//function displayViolationMapList( obj, id, lineBreak ) {
-//    var violationsText = "";
-//    var isViolations = true;
-//
-//    if ( typeof obj !== "undefined" ) {
-//        for ( var o in obj ) {
-//            if( obj.hasOwnProperty( o ) ) {
-//                violationsText += o + ": ";
-//                obj[o].forEach( function( s ) {
-//                    violationsText += s + ", ";
-//                } );
-//                violationsText += "<br>" + ( !!lineBreak ? "<br>" : "" );
-//            }
-//        }
-//    } else {
-//        isViolations = false;
-//    }
-//
-//    displayViolation( id, isViolations, violationsText );
-//}
-//
-//function displayViolationList( list, id, lineBreak ) {
-//    var violationsText = "";
-//    var isViolations = true;
-//
-//    if ( typeof list !== "undefined" ) {
-//        list.forEach( function( violation ) {
-//            violationsText += violation + "<br>" + ( !!lineBreak ? "<br>" : "" );
-//        } );
-//    } else {
-//        isViolations = false;
-//    }
-//
-//    displayViolation( id, isViolations, violationsText );
-//}
-//
-//
-//function displayViolation( id, isViolation, violationsText ) {
-//
-//    var $button = $( "#" + id + "Button" );
-//    var $container = $( "#" + id + "Div" );
-//
-//    if ( isViolation ) {
-//        $button
-//            .attr( { src: "../images/down.png", title: "View violations" } )
-//            .css( { cursor: "pointer" } )
-//            .click( function() { $container.slideToggle( "fast" ); } );
-//
-//        $container.html( violationsText );
-//    } else {
-//        $button.attr({ src: "../images/check.png", title: "No violations" } );
-//    }
-//
-//    $container.hide();
-//}
