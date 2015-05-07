283ed5fe89eada61a2d9817ff77b96ace988cc86
Jan Henrik Overland <janhenrik.overland@gmail.com>
2015-05-07 12:48:02 +0200

    er plugin

--- dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/plugin/eventreport.js
+++ dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/plugin/eventreport.js
@@ -2234,7 +2234,7 @@ Ext.onReady( function() {
 					return null;
 				}
 
-				if (component.mask) {
+				if (component.mask && component.mask.destroy) {
 					component.mask.destroy();
 					component.mask = null;
 				}
